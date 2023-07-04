e2daeeac1720d4218bd6f96e221dbe9b20196870
ajgeiss0702 <ajgeiss72@gmail.com>
2023-07-03 19:09:27 -0500

    start fetching records before awaiting currentYear

--- src/routes/history/+page.ts
+++ src/routes/history/+page.ts
@@ -3,8 +3,8 @@ import type {PageLoad} from "./$types";
 export const load = (async ({fetch}) => {
     return {
         history: {
-            currentYear: await fetch("/api/history/year/" + new Date().getFullYear()).then(r => r.json()),
-            records: fetch("/api/history/records").then(r => r.json())
+            records: fetch("/api/history/records").then(r => r.json()),
+            currentYear: await fetch("/api/history/year/" + new Date().getFullYear()).then(r => r.json())
         }
     }
 }) satisfies PageLoad;
