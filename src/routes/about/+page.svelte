78c7a288025dc067b15b4cc328885b9b30aac59e
ajgeiss0702 <ajgeiss72@gmail.com>
2023-07-16 16:20:13 -0500

    fix layout shifting on about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -17,7 +17,7 @@
             <br>
             <div class="text-center w-full">
                 <h2 class="text-center">Why does the website say 4:30?</h2>
-                <img class="mx-auto" src="/430.webp" alt="Dan confirming 4:30"/>
+                <img class="mx-auto danwan" src="/430.webp" alt="Dan confirming 4:30"/>
                 <i>always has been</i>
             </div>
 
@@ -32,3 +32,8 @@
 <div class="absolute bottom-0 right-0 p-2">
     <a href="/">Back to Countdown</a>
 </div>
+<style>
+    .danwan {
+        height: 60px;
+    }
+</style>
