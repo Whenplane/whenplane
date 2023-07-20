b275999c4b9eaa6bb6f08a685038255e987a75e0
ajgeiss0702 <ajgeiss72@gmail.com>
2023-07-19 19:45:43 -0500

    explain wait in history load function

--- src/routes/history/+page.ts
+++ src/routes/history/+page.ts
@@ -1,10 +1,12 @@
 import type {PageLoad} from "./$types";
-import {wait} from "../../lib/utils";
+import {wait} from "$lib/utils";
 
 export const load = (async ({fetch}) => {
     const records = fetch("/api/history/records").then(r => r.json());
     const currentYear = await fetch("/api/history/year/" + new Date().getFullYear()).then(r => r.json());
 
+    // only wait 50ms extra for records. If they aren't done fetching by then,
+    //  just return the promise so sveltekit can stream it when it does finish
     const returned = await Promise.any([records, wait(50)]);
 
     const recordsDone = !!returned;
