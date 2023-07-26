f2cf75de0b5bbb2cd6c403a5be2120d33687575c
ajgeiss0702 <ajgeiss72@gmail.com>
2023-07-25 19:03:23 -0500

    Add WAN thumbnail on error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,14 +1,33 @@
 <script>
     import {page} from "$app/stores";
+
+    let alt = "\"This is Awkward\" - Thumbnail from March 21st, 2021"
 </script>
 
 <div class="text-center">
+    <img src="/this_is_awkward.jpg" {alt} title={alt}/>
     <h1>{$page.status}</h1>
     {$page.error.message}
 </div>
 
 <style>
     h1 {
+        margin-top: 2rem;
+    }
+    img {
         margin-top: 4rem;
+        @apply mx-auto;
+        height: 50vh;
+    }
+    
+    @media (max-width: 95vh) {
+        img {
+            height: auto;
+            max-width: 90vw;
+        }
+    }
+
+    :global(.dark) img {
+        background-color: black;
     }
 </style>
