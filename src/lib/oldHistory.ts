d9821b9761547e468b03db7a2db4307cd5719934
ajgeiss0702 <ajgeiss72@gmail.com>
2023-07-28 12:08:39 -0700

    Add extra metadata for old shows that have records

--- src/lib/oldHistory.ts
+++ src/lib/oldHistory.ts
@@ -68,7 +68,40 @@ export const history: HistoricalEntry[] = [
         metadata: {
             preShowStart: "2023-04-08T00:37:59.000Z",
             mainShowStart: "2023-04-08T00:51:19.000Z",
-            showEnd: "2023-04-08T06:07:51.000Z"
+            showEnd: "2023-04-08T06:07:51.000Z",
+            title: "A Monster Lawsuit Is Coming For Me",
+            vods: {
+                floatplane: "lBhG8ZtB25",
+                youtube: "s-llzV_3LaE"
+            },
+            "snippet": {
+                "publishedAt": "2023-04-08T06:38:30Z",
+                "channelId": "UCXuqSBlHAE6Xw-yeJA0Tunw",
+                "title": "A Monster Lawsuit Is Coming For Me - WAN Show April 7, 2023",
+                "description": "Don't just browse the web – build it. Apply for free today using the link https://covalence.io/wan and take your first step toward a career in software development with Covalence.\n\nTry Zoho One free for 30 days with no credit card required here: https://www.zoho.com/one/lp/linus.html\n\nVisit https://www.squarespace.com/WAN and use offer code WAN for 10% off\n\nPodcast Download: https://spotifyanchor-web.app.link/e/WDqFGhZtTyb\n\nTimestamps: (Courtesy of NoKi1119)\n0:00 Chapters\n1:32 Intro\n2:33 Topic #1 - Monster Energy sues for a game having \"monster\" in title\n3:04 Comparing logos, company sued for using the color green\n4:32 Linus discusses Monster Energy drinks & the incentive\n6:36 Monster Fish Keepers, Luke on abusing trademarks\n8:54 Recalling Kylie lawsuit, Brookhaven Bears, the point behind trademarks\n10:25 Luke on documented confusion of Linus with Linux\n11:47 Linus recalls Tech Sack's past name\n12:18 Monster sued Ubisoft, Luke on game names & Unity demos\n13:33 Dennis hijacks WAN Show using Uncle Linus's Sack\n15:22 Linus reads Uncle Linus's Potato Farm letter, starts the competition\n22:02 Linus did not plan this, Dennis says the answers\n24:44 Luke calls out someone for flexing with British food\n25:46 Linus's preference, Luke on \"Cold mall fry,\" Dennis's drive\n27:06 Topic #2 - TechLinked temporarily deleted\n28:37 Unrestored comments, Linus mentions past hack\n29:32 Luke's Content ID theory, Google's lack of warnings\n33:24 Linus on Floatplane transcoding & writing \"www\"\n36:22 Topic #3 - Intel's Arc Battlemage & Celestial\n36:46 Luke dislikes Linus's habit, reporting rumors & NVIDIA\n40:41 Luke recalls Arc challenge, \"review GPUs before it comes out\" idea\n42:46 Linus on predictive content, LTT's artificial CPU pre-review\n46:02 Topic #4 - Luke fragmented the infected M2 SSD\n46:53 Luke missed hitting it with a bat\n49:14 Specs of the SSD, discussing malware\n50:18 Luke freaked out as CTO, Linus on the motherboard's cost\n52:48 Luke on lack of spare systems, Jaiden works on a Steam Deck\n55:38 Standardizing workstations, reliability of laptops\n1:00:02 Linus & Luke on the feasibility of 10GbE & workstation specs\n1:06:26 Showing the server graveyard, planned LTT vlogs\n1:08:24 What does \"Linus Media Group\" do as a company?\n1:13:01 Linus on the badminton center's name, Luke on Geeksquad\n1:16:51 Linus asks ChatGPT, gets \"Smash Club,\" Luke's suggestion\n1:19:29 Black lights, partnership, Luke on \"Smash Club\" & domains\n1:24:37 Linus's LTX stage idea\n1:27:20 Topic #5 - Starforged Systems's parody video\n1:38:15 SAMMIT's video, references, Luke's habit, LTT fidget toy\n1:41:22 What Linus & Luke watched, SAMMIT invited Linus to drifting\n1:46:22 Merch Messages #1\n1:58:51 Any fun anecdotes while making the Uncle Linus video?\n2:04:14 Sponsor - Covalence ft. Linus's Google rant\n2:06:44 Sponsor - Zoho One\n2:07:52 Sponsor - Squarespace ft. \"Imported Juice Exchange\"\n2:10:30 Topic #6 - Artificial Intelligence update\n2:11:16 Microsoft's Jarvis, mentions Wolfram|Alpha\n2:13:49 HuggingGPT, OpenAI sued for defamation\n2:15:52 Topic #7 - VR adoption is slow amongst teens\n2:19:02 Linus on Gen Z, Luke's past with consoles & present\n2:23:17 Topic #8 - General Motors removes Android Auto & Apple CarPlay\n2:26:55 LTTStore's new windbreaker\n2:33:14 LTTStore's new neon motherboard T-shirt, dad hats not selling well\n2:35:46 Luke reviews the LTTStore laptop bag\n2:44:01 Merch Messages #2\n2:46:51 Best paint for PC cases?\n2:55:52 Topic #9 - Reusing thumbnails & faces for LTT videos\n3:01:08 Topic #10 - Microsoft emulator loophole patched\n3:06:07 Topic #11 - WD My Cloud locked due to breach\n3:06:38 Linus discusses his investment in the NAS company\n3:14:13 Topic #12 - Samsung data leak using ChatGPT\n3:14:54 Luke's predicts the return of local servers & systems\n3:19:37 Topic #13 - Next \"smart\" devices remotely controlled over the internet\n3:22:56 Merch Messages #3 ft. WAN Show after dark\n3:24:11 Luke's opinion on people wanting to stop AI's development\n3:28:02 Has LTT or Floatplane had to overcome legal hurdles? ft. AI talk\n3:38:51 Features you want on a hand-held high quality holster?\n3:41:20 Game mechanics you'd like to see AI applied to? ft. Google Translate\n3:57:51 Most surprising feature on a new GP? recovering mentally\n4:07:38 What is Linus', Lukes' & Dans' SO's biggest taboo?\n4:18:08 Luke's thoughts on Adam Conover's AI scam video\n4:23:06 Full on gaming LMG channel? ft. pending approval Labs logo\n4:24:42 What is the best resource to educate seniors in law about AI?\n4:31:36 Possible accent colors for LTT screwdriver?\n4:39:50 Computer issue that annoyed you until you fixed it?\n4:43:29 Luke on Floatplane development & blog content ft. favorite keyboards\n4:51:28 Linus on bridging internet connections\n4:54:04 Linus on privateering unlisted LMG videos exposed by hackers\n4:58:09 Open sourcing Labs software? ft. Floatplane updates, LTTStore questions\n5:15:52 Outro",
+                "thumbnails": {
+                    "maxres": {
+                        "url": "https://i.ytimg.com/vi/s-llzV_3LaE/maxresdefault.jpg",
+                        "width": 1280,
+                        "height": 720
+                    }
+                },
+                "channelTitle": "Linus Tech Tips",
+                "tags": [
+                    "pcmr",
+                    "building",
+                    "competition",
+                    "gamers",
+                    "how to",
+                    "review",
+                    "intel",
+                    "amd",
+                    "cpu",
+                    "gpu",
+                    "gaming"
+                ],
+                "categoryId": "28"
+            }
         }
     },
     {
@@ -220,7 +253,40 @@ export const history: HistoricalEntry[] = [
         metadata: {
             preShowStart: "2022-11-18T23:06:18.000Z",
             mainShowStart: "2022-11-18T23:33:29.000Z",
-            showEnd: "2022-11-19T02:04:16.000Z"
+            showEnd: "2022-11-19T02:04:16.000Z",
+            title: "We've Made Some Big Mistakes",
+            vods: {
+                floatplane: "MaleYCjy2L",
+                youtube: "vKZXiQOO52I"
+            },
+            "snippet": {
+                "publishedAt": "2022-11-19T02:13:26Z",
+                "channelId": "UCXuqSBlHAE6Xw-yeJA0Tunw",
+                "title": "We've Made Some Big Mistakes - WAN Show November 18, 2022",
+                "description": "Get Zerto Disaster Recovery and Replication by OVHcloud free for 30 days at https://bit.ly/3OfTu5W\nVessi is giving away a pair of socks to the first 100 shoes sold using code SocksWanshow at https://Vessi.com/wanshow\nCheck out 45Drives Storage Solutions: https://lmg.gg/45DrivesWAN\n\nPodcast Download: https://spotifyanchor-web.app.link/e/CBXzELqW8ub\n\nTimestamps: (Courtesy of NoKi1119)\n0:00 Chapters\n1:48 Intro\n2:13 Topic #1 - Mistakes on LTT's RTX 4080 video\n2:48 4080 sold out, pricing, advocating for change\n8:15 Linus counters community arguments\n13:32 High last-gen emphasis in the review\n14:46 Lack of AMD discussions in the review\n15:29 Linus apologizes, reasons his decisions\n16:36 AMD cards not priced well, discussing competition\n19:07 Topic #2 - Linus & Luke 30 day Arc challenge\n19:24 Linus's & Luke's power \"mods\"\n22:46 Linus's disappointment on VR, reaching staff\n25:04 Luke's idea to Linus's Arc & Linux systems\n26:33 Linus & Luke streamed gaming on Arc\n27:44 Encoding problems on Arc, stable on CPU\n28:32 LTTStore customer support delays ft. Luke's bird\n30:16 Labeled shipments show as shipping when not\n32:45 LTTStore's Retro Polar Fleece\n33:44 Topic #3 - The last seven days in Twitter\n34:32 Luke on companies firing/hiring\n37:36 Engineer corrects Elon, gets fired\n40:30 Elon & California’s at-will employment\n41:52 Luke meets Twitter employee in Serbia\n42:42 Linus on at-will, Luke hushes birds\n44:02 Badge system removed, \"public\" Slack channel\n45:36 Blind, a better localized anonymous version of LinkedIn\n47:16 Elon response, end-to-end DM encryption\n49:34 \"Be extremely hardcore or leave\"\n52:12 Employees locked out after blue badge removal\n52:28 Sponsor - OVHcloud\n53:54 Sponsor - Vessi Footwear, \"waterproof\" shoes\n55:36 Companies should hire employees, discussing Visas\n56:18 Discussing Luke's bird & feeding\n57:04 Sponsor - 45Drives\n57:44 Linus to head off to an event\n58:22 LMG & Creators Warehouse hiring many positions\n59:28 Topic #4 - FTX collapses, Riley writing in the doc\n1:01:06 Merch Messages #1\n1:01:36 Luke on dive computers\n1:03:37 Any advantage to 2 PCI-Es on larger GPUs?\n1:05:40 LTTStore banana for scale on catnip?\n1:05:54 Riley takes over, everything goes downhill\n1:09:26 Riley on writing the doc, Luke on coverage\n1:12:04 Riley on Twitter's viability & usability\n1:14:20 Discussing Andor\n1:15:26 Riley hopes another \"Twitter\" arrives\n1:16:24 Discussing Taskmaster\n1:18:15 Luke explains FTX, owner playing LOL during conference calls\n1:21:06 Binance backed out bailing FTX out\n1:23:10 Regulating cryptocurrency defeated its purpose\n1:26:42 Luke on losing coins VS losing value\n1:28:42 Topic #5 - Pokémon Scarlet and Violet\n1:29:32 Luke's favorite Pokémon\n1:32:58 Game graphics & performance issues, Switch & emulation\n1:39:49 Topic #6 - DOOM Eternal OST controversy\n1:41:04 Summarizing OST release, Marty's reddit post\n1:43:25 Mick Gordon's detailed rebuttal, game developing\n1:48:44 Luke on gaming scenes & gamers review-bombing\n1:52:38 Topic #7 - PeopleMakeGames calls Valve out on gambling\n1:53:32 PMG's video on Valve gambling\n1:54:48 Twitch banning unregulated gambling content\n1:56:56 CS:GO gambling sites thrive, discussing loot boxes\n1:59:36 Why doesn't Valve do anything against this?\n2:01:32 AK-47 Case Hardened #661 sold for $400,000\n2:04:02 Whose responsibility is this? ft. Luke hushing birds\n2:09:16 Video Game Attorney on platforms manipulating drops\n2:11:28 Topic #8 - Apple sued for collecting data in Apple apps\n2:14:54 Riley on Apple misleading users & lack of privacy\n2:16:12 Past Canada real-estate collapse & \"audited\" houses\n2:17:16 Did Luke believe Apple's security?\n2:20:28 Merch Messages #2\n2:20:48 Using external GPUs for Blender\n2:22:17 IS RDNA3 price competitive for Blender?\n2:22:54 ID security suggestions\n2:25:16 Would Linus(Riley) & Luke consider skydiving?\n2:30:14 Outro",
+                "thumbnails": {
+                    "maxres": {
+                        "url": "https://i.ytimg.com/vi/vKZXiQOO52I/maxresdefault.jpg",
+                        "width": 1280,
+                        "height": 720
+                    }
+                },
+                "channelTitle": "Linus Tech Tips",
+                "tags": [
+                    "pcmr",
+                    "building",
+                    "competition",
+                    "gamers",
+                    "how to",
+                    "review",
+                    "intel",
+                    "amd",
+                    "cpu",
+                    "gpu",
+                    "gaming"
+                ],
+                "categoryId": "28",
+            }
         }
     },
     {
@@ -392,31 +458,34 @@ export const history: HistoricalEntry[] = [
 
 export type HistoricalEntry = {
     name: string,
-    metadata: {
-        preShowStart: string,
-        mainShowStart: string,
-        showEnd: string,
-        title?: string,
-        vods?: {
-            floatplane?: string,
-            youtube: string
+    metadata: OldShowMeta,
+    value?: OldShowMeta
+}
+
+type OldShowMeta = {
+    preShowStart: string,
+    mainShowStart: string,
+    showEnd: string,
+    title?: string,
+    vods?: {
+        floatplane?: string,
+        youtube: string
+    },
+    snippet?: {
+        publishedAt: string,
+        channelId: string,
+        title: string,
+        description?: string,
+        thumbnails: {
+            default?: YoutubeThumbnail,
+            medium?: YoutubeThumbnail,
+            high?: YoutubeThumbnail,
+            standard?: YoutubeThumbnail,
+            maxres: YoutubeThumbnail
         },
-        snippet?: {
-            publishedAt: string,
-            channelId: string,
-            title: string,
-            description?: string,
-            thumbnails: {
-                default?: YoutubeThumbnail,
-                medium?: YoutubeThumbnail,
-                high?: YoutubeThumbnail,
-                standard?: YoutubeThumbnail,
-                maxres: YoutubeThumbnail
-            },
-            channelTitle: string,
-            tags?: string[],
-            categoryId?: string
-        }
+        channelTitle: string,
+        tags?: string[],
+        categoryId?: string
     }
 }
 
