e7a65cbfafb14f304dfb4cc737e5eaf2bd191537
ajgeiss0702 <ajgeiss72@gmail.com>
2023-07-28 11:03:22 -0700

    Add endpoint to get show meta *and* value

---
+++ src/routes/api/history/show/[...showDate]/+server.ts
@@ -0,0 +1,27 @@
+import type {RequestHandler} from "@sveltejs/kit";
+import {error, json} from "@sveltejs/kit";
+
+export const GET = (async ({platform, params}) => {
+
+    const history = platform?.env?.HISTORY;
+    if(!history) throw error(503, "History missing!");
+
+    const kvShowInfo = await history.getWithMetadata(params.showDate, {type: 'json'});
+
+    if(kvShowInfo.value) {
+        return json({
+            name: params.showDate,
+            metadata: kvShowInfo.metadata,
+            value: kvShowInfo.value
+        });
+    }
+
+    const oldHistory = await import("$lib/oldHistory");
+    for (const oldShow of oldHistory.history) {
+        if(oldShow.name == params.showDate) {
+            return json(oldShow);
+        }
+    }
+
+    throw error(404, "Show not found")
+}) satisfies RequestHandler;
