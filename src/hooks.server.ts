8eb1d871eb68d855e6d9813cce07ed23639ecfb1
ajgeiss0702 <ajgeiss72@gmail.com>
2023-08-24 13:15:33 -0700

    Set no-store on /

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -78,5 +78,9 @@ export const handle: Handle = async ({ event, resolve }) => {
         }
     }
 
+    if(event.url.pathname === "/") {
+        response.headers.set("Cache-Control", "no-store, max-age=0");
+    }
+
     return response;
 }
