3cf82bcdc97cd2d742bf3b2afb634de20071afdd
ajgeiss0702 <ajgeiss72@gmail.com>
2023-09-06 22:11:37 -0700

    No ads

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -15,7 +15,7 @@
 </svelte:head>
 <div class="container h-full mx-auto flex justify-center items-center limit">
     <div class="space-y-5">
-        <div class="card p-4 inline-block text-left m-2">
+        <div class="card p-4 inline-block text-left m-2 relative bottom-12">
             <h1 class="text-center">About</h1>
             This website was made so it can be easier to tell when the WAN is starting, without having to go back between twitch/youtube.
             <br>
@@ -31,10 +31,16 @@
             <br>
             <br>
             <div class="text-center w-full">
-                <h2 class="text-center">Why does the website say 4:30?</h2>
+                <h2 class="text-center">Why does it say WAN starts at 4:30?</h2>
                 <img class="mx-auto danwan" src="/430.webp" alt="Dan confirming 4:30"/>
                 <i>always has been</i>
             </div>
+            <br>
+            <div class="text-center w-full">
+                <h2 class="text-center">Will there ever be ads?</h2>
+                No. Ads suck.
+            </div>
+            <br>
 
             <div class="text-right">
                 <a class="btn variant-ghost-primary" href="https://about.ajg0702.us" target="_blank" rel="noreferrer">
