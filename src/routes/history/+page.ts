562771030a2499bf9df260bd57c551cf9e5dddfb
ajgeiss0702 <ajgeiss72@gmail.com>
2023-09-15 22:33:00 -0700

    cache bust historical shows around wan time

--- src/routes/history/+page.ts
+++ src/routes/history/+page.ts
@@ -4,9 +4,18 @@ import { browser } from "$app/environment";
 import { getCookie } from "$lib/cookieUtils";
 
 export const load = (async ({fetch, params, url}) => {
+    const now = new Date();
+
+    let cacheBusting;
+    if((now.getUTCDay() === 5 && now.getUTCHours() > 19) || (now.getUTCDay() === 6 && now.getUTCHours() < 10)) {
+        cacheBusting = "?d=" + now.getTime();
+    } else {
+        cacheBusting = "";
+    }
+
     const oldHistory = url.searchParams.has("old") ? import("$lib/oldHistory") : undefined;
     const records = fetch("/api/history/records").then(r => r.json());
-    const currentYear = await fetch("/api/history/year/" + new Date().getFullYear()).then(r => r.json());
+    const currentYear = await fetch("/api/history/year/" + now.getFullYear() + cacheBusting).then(r => r.json());
 
     // only wait 50ms extra for records. If they aren't done fetching by then,
     //  just return the promise so sveltekit can stream it when it does finish
