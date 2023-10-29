76227236a96a27c5fe0c3ad5c431084a181721d4
ajgeiss0702 <ajgeiss72@gmail.com>
2023-10-29 10:58:53 -0700

    hacky way of doing this since sveltekit doesnt like searchParam arguments in prerendered data

--- src/routes/history/missing/times/+page.server.ts
+++ src/routes/history/missing/times/+page.server.ts
@@ -1,10 +1,11 @@
 import type { HistoricalEntry } from "$lib/utils.ts";
+import {GET as oldShowsGet} from "../../../api/oldShows/+server.ts"
 
 
-export const load = (async ({fetch}) => {
+export const load = (async () => {
 
   return {
-    oldShows: await fetch("/api/oldShows?removeDescription").then(r => r.json()) as HistoricalEntry[]
+    oldShows: await oldShowsGet({url: new URL("https://prerender/api/oldShows?removeDescription")}).then(r => r.json()) as HistoricalEntry[]
   }
 
 }) satisfies PageServerLoad;
