c30e00ca21141f99a640525ef3f7430ecd3cac23
ajgeiss0702 <ajgeiss72@gmail.com>
2023-12-02 00:18:28 -0700

    Add privacy thing since that might be required for the upcoming extension

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,85 @@
+<div class="limit mx-auto mt-16 mb-32">
+  <h1>Privacy Policy for Whenplane (whenplane.com)</h1>
+  <br>
+  <br>
+  <div class="card p-3">
+    <h2>TL;DR</h2>
+    Whenplane doesn't collect much, and what it does collect will not be shared other than aggregate stats.<br>
+    <br>
+    For example, the data may be used to figure out how many unique users used the site in a given time period.
+    This number of unique users may be shared.<br>
+    The data that lead to the calculation of those numbers will not be shared outside of the place they are stored for any reason.<br>
+    <br>
+    The only potentially sensitive information that Whenplane saves is your User Agent, which may describe what browser and operating system you are using.<br>
+  </div>
+
+  <br>
+
+  Last Updated: {new Date( 1701500392264 ).toLocaleDateString(undefined, {dateStyle: "medium"})}
+
+  <br>
+  <br>
+
+  <p>Whenplane is committed to protecting the privacy of its users. This Privacy Policy outlines the information
+    collected by Whenplane and how that information is used. By using the Whenplane website, you agree to the terms
+    outlined in this policy.</p>
+
+  <h2>1. Information Collected</h2>
+
+  <h3>1.1 Analytics Data</h3>
+  <p>Whenplane collects non-personally identifiable information for the purpose of improving our services and user
+    experience. This data may include:</p>
+  <ul style="list-style: initial;" class="ml-6">
+    <li>A unique user identifier, which is randomized every 25-35 days</li>
+    <li>Browser type and version</li>
+    <li>Operating system type and version</li>
+    <li>Pages visited on the website</li>
+    <!-- Add more items as necessary -->
+  </ul>
+
+  <h3>1.2 Randomly Generated Identifier</h3>
+  <p>To enhance user privacy, Whenplane assigns a randomly generated identifier to each user. This identifier is used
+    for analytics purposes only and is not linked to any personally identifiable information. The identifier is
+    periodically reset every 25-35 days to further protect user anonymity.</p>
+
+  <h2>2. Use of Information</h2>
+
+  <h3>2.1 Analytics</h3>
+  <p>The information collected is used to analyze user behavior, improve our website, and enhance user experience. This
+    may include optimizing features, understanding user preferences, and identifying areas for improvement.</p>
+
+  <h3>2.2 Cookies</h3>
+  <p>Whenplane may use cookies to collect and store information. Users have the option to disable cookies in their
+    browser settings; however, this may impact the functionality of certain features on the website.</p>
+
+  <h2>3. Data Sharing</h2>
+  <p>Whenplane does not sell, trade, or otherwise transfer user information to external parties. We may share
+    non-personally identifiable information with trusted third-party service providers who assist us in operating
+    our website and conducting our business, as long as these parties agree to keep the information
+    confidential.</p>
+
+  <h2>4. Security</h2>
+  <p>Whenplane employs industry-standard security measures to protect user information. However, no method of
+    transmission over the internet or electronic storage is 100% secure, and while we strive to use commercially
+    acceptable means to protect your personal information, we cannot guarantee its absolute security.</p>
+
+  <h2>5. Changes to the Privacy Policy</h2>
+  <p>Whenplane reserves the right to update this Privacy Policy at any time. Users will be notified of any changes on
+    this page. It is advisable to review this page periodically for any updates.</p>
+
+  <h2>6. Contact Information</h2>
+  <p>If you have any questions or concerns regarding this Privacy Policy, you may contact us at
+    <a href="mailto:contact@whenplane.com">contact@whenplane.com</a>.</p>
+
+  <p>By using Whenplane, you signify your acceptance of this Privacy Policy. If you do not agree to this policy,
+    please do not use our website.</p>
+
+  Effective Date: {new Date(1701500392264).toLocaleDateString(undefined, {dateStyle: "medium"})}
+</div>
+
+<style>
+  p {
+      margin-top: 0.25em;
+      margin-bottom: 0.25em;
+  }
+</style>
