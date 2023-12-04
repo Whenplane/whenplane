a40f2a60723ea41c5bddbe4c5202750c9851dc34
ajgeiss0702 <ajgeiss72@gmail.com>
2023-12-03 17:52:51 -0700

    add support page

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,4 @@
+<div class="text-center mt-16">
+  <h1>Support</h1>
+  To get support with anything to do with whenplane, feel free to email <a href="mailto:support@whenplane.com">support@whenplane.com</a>
+</div>
