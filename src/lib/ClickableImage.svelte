c69fb0c28bf8d841914416c81c21960b0ce2250f
ajgeiss0702 <ajgeiss72@gmail.com>
2023-12-03 18:27:35 -0700

    Add extension page

---
+++ src/lib/ClickableImage.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let src;
+  export let margin = "m-2";
+</script>
+<a href={src} class={margin} target="_blank" rel="noopener">
+  <img {src} {...$$restProps}>
+</a>
+
+<style>
+  img {
+      display: inline-block;
+      border: rgba(255, 255, 255, 0.5) solid 2px;
+      border-radius: 8px;
+  }
+</style>
---
+++ src/routes/extension/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+  import ClickableImage from "$lib/ClickableImage.svelte";
+</script>
+
+<div class="limit-large mx-auto mt-16">
+  <h1>Whenplane Floatplane Widget Extension</h1>
+  For you fellow Floatplane watchers, I've made a browser extension that replaces the "Offline" screen on the LTT floatplane channel with whenplane.
+  <br>
+  <br>
+
+  <div class="text-center">
+    <a
+      class="btn variant-filled-primary"
+      href="https://addons.mozilla.org/en-US/firefox/addon/whenplane-floatplane-widget/"
+      target="_blank"
+      rel="noopener"
+    >
+      <img src="/extension/browser-icons/firefox.svg" class="browser-icon">
+      Download for Firefox
+    </a>
+    <a
+      class="btn variant-filled-secondary"
+      href="https://chromewebstore.google.com/detail/whenplane-floatplane-widget/jiijhgibeiaaacbohdjihncoingomjjm"
+      target="_blank"
+      rel="noopener"
+    >
+      <img src="/extension/browser-icons/chrome.svg" class="browser-icon">
+      Download for Chrome
+    </a>
+    <br>
+    <br>
+    <br>
+    <ClickableImage src="/extension/screenshots/late.png" style="width: 48%;"/>
+    <ClickableImage src="/extension/screenshots/after.png" style="width: 48%;"/>
+  </div>
+</div>
+
+<style>
+  .browser-icon {
+      height: 2em;
+      border-radius: 100%;
+      @apply mx-2;
+  }
+</style>
