0911dd37214c5a1810b799245789797f205564e1
ajgeiss0702 <ajgeiss72@gmail.com>
2023-12-15 15:00:21 -0700

    whoops

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -9,7 +9,7 @@
         // Auto-reload if root page throws a 500. This usually just happens because the data request failed. Thanks sveltekit..
         setTimeout(() => {
             const newUrl = new URL(location.href);
-            newUrl.searchParams.set(attempt, ++attempt)
+            newUrl.searchParams.set("attempt", ++attempt)
             location.href = newUrl.toString();
         }, Math.min(Math.max(3e3, 3e3 * (attempt / 3)), 30e3));// delay by 3-30 seconds so that we don't spam reloads if the site is actually broken
     }
