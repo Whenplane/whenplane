7313b25519101a1199badb18c7f3f5b2e6ea9586
ajgeiss0702 <ajgeiss72@gmail.com>
2023-12-16 17:24:42 -0700

    Update extension version

--- src/routes/extension/+page.svelte
+++ src/routes/extension/+page.svelte
@@ -43,7 +43,7 @@
       Download for Chrome
     </a>
     <br>
-    or <a href="https://downloads.whenplane.com/whenplane_floatplane_widget-1.0.3.4.zip" download>download zip</a>
+    or <a href="https://downloads.whenplane.com/whenplane_floatplane_widget-1.0.4.zip" download>download zip</a>
     <br>
     <br>
     <a
