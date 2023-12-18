765f61a7e0a2b7292eb8b75ceecf66f41576a298
ajgeiss0702 <ajgeiss72@gmail.com>
2023-12-18 12:54:21 -0700

    Fixed bottom of history page not loading the old shows Only happened if you didnt have to scroll at all, the browser just put you there. Probably only happens in dev but might as well fix it ig

--- src/routes/timeExtractor/testSharp/img/+server.ts
+++ src/routes/timeExtractor/testSharp/img/+server.ts
@@ -3,7 +3,7 @@ import { error } from "@sveltejs/kit";
 
 export const GET = (async () => {
   if(!dev) throw error(503, "Not available in prod!");
-  const image = await fetch("https://i.ytimg.com/vi/TXsw_92Y2e0/maxresdefault.jpg")
+  const image = await fetch("https://i.ytimg.com/vi/4QUzf-J0w_o/maxresdefault_live.jpg")
     .then(r => r.arrayBuffer());
   const buffer = await ((await import("sharp")).default)(image)
     .flatten()
