750233fb08d93f7ca0df3aacaccf5a1f1c70b25c
ajgeiss0702 <ajgeiss72@gmail.com>
2023-12-21 14:12:10 -0700

    and guests

--- src/routes/api/specialStream/+server.ts
+++ src/routes/api/specialStream/+server.ts
@@ -6,7 +6,7 @@ export const GET = (async () => {
   // In the future this will be from a database, but this will be fine for now.
 
   const data: SpecialStream = {
-    title: "Gingerbread PC Building (with Sarah)",
+    title: "Gingerbread PC Building (with Sarah & guests)",
     thumbnail: "https://images.ajg0702.us/gingerbread_sarah.jpeg",
 
     start: "2023-12-21T20:30:00.000Z",
