f90ed6726945d26cc13e00810dfca61f19d5ead7
ajgeiss0702 <ajgeiss72@gmail.com>
2024-01-12 15:03:22 -0600

    increase reload number too

--- src/routes/api/(live-statuses)/aggregate/+server.ts
+++ src/routes/api/(live-statuses)/aggregate/+server.ts
@@ -68,7 +68,7 @@ export const GET = (async ({url, fetch, locals, platform}) => {
         hasDone: await hasDone,
         votes: await votes,
         specialStream: await specialStream,
-        reloadNumber: 0
+        reloadNumber: 1
         // showExtension: await showExtension
     }
 
