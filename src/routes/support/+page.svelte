f536c2a83d09767483840ebc9d5ec2ebe425a4c2
ajgeiss0702 <ajgeiss72@gmail.com>
2024-01-21 09:20:58 -0700

    add discord to support page

--- src/routes/support/+page.svelte
+++ src/routes/support/+page.svelte
@@ -1,4 +1,10 @@
-<div class="text-center mt-16">
+<div class="limit mx-auto text-center mt-16">
   <h1>Support</h1>
   To get support with anything to do with whenplane, feel free to email <a href="mailto:support@whenplane.com">support@whenplane.com</a>
+  <br>
+  <br>
+  <h2>Discord</h2>
+  You can also contact the author directly on discord.<br>
+  My username is <code>ajgeiss0702</code>. I do not accept friend requests, and you need to be in a shared server to message me.
+  I am in the <a href="https://lmg.gg/discord">LTT Discord</a> if you dont already share a discord server with me.
 </div>
