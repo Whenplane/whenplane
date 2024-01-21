e515f86032a7ae842143bc5e8cbb44d8b68ffc46
ajgeiss0702 <ajgeiss72@gmail.com>
2024-01-21 09:24:20 -0700

    add breadcrumbs to support page

--- src/routes/support/+page.svelte
+++ src/routes/support/+page.svelte
@@ -1,3 +1,13 @@
+<script>
+import { page } from "$app/stores";
+</script>
+
+<ol class="breadcrumb pt-2 pl-2">
+  <li class="crumb"><a class="anchor hover-underline" href="/">{$page.url.hostname === "whenwan.show" ? "whenwan.show" : "Whenplane"}</a></li>
+  <li class="crumb-separator" aria-hidden="true">&rsaquo;</li>
+  <li class="crumb">Support</li>
+</ol>
+
 <div class="limit mx-auto text-center mt-16">
   <h1>Support</h1>
   To get support with anything to do with whenplane, feel free to email <a href="mailto:support@whenplane.com">support@whenplane.com</a>
@@ -6,5 +16,7 @@
   <h2>Discord</h2>
   You can also contact the author directly on discord.<br>
   My username is <code>ajgeiss0702</code>. I do not accept friend requests, and you need to be in a shared server to message me.
-  I am in the <a href="https://lmg.gg/discord">LTT Discord</a> if you dont already share a discord server with me.
+  I am in the
+  <a href="https://lmg.gg/discord" target="_blank" rel="noopener">LTT Discord</a>
+  if you dont already share a discord server with me.
 </div>
