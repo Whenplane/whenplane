8b8455632a8369a23e07185e5d58d3d28c558f55
ajgeiss0702 <ajgeiss72@gmail.com>
2024-01-21 09:02:08 -0700

    ah

--- src/routes/tools/test/LatenessVotingTest.svelte
+++ src/routes/tools/test/LatenessVotingTest.svelte
@@ -2,19 +2,20 @@
   import type { MainLate } from "$lib/utils.ts";
   import { timeString } from "$lib/timeUtils.ts";
   import LatenessVoting from "$lib/LatenessVoting.svelte";
+  import { writable } from "svelte/store";
 
   let lateness = 0;
 
-  let mainLate: MainLate;
-  $: mainLate = {
+  let mainLate = writable<MainLate>();
+  $: mainLate.set({
     isMainLate: true,
     late: true,
     distance: lateness,
     string: timeString(lateness)
-  }
+  })
 </script>
 
 <input type="range" class="input" min="0" max={24 * 60 * 60e3} step="1000" bind:value={lateness}>
-{mainLate.string}
+{$mainLate.string}
 
 <LatenessVoting {mainLate}/>
