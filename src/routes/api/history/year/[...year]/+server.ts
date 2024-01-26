a9beb4edd0de44eab594710952e4cea60f2563da
ajgeiss0702 <ajgeiss72@gmail.com>
2024-01-26 08:40:08 -0700

    do separate year lists concurrently

--- src/routes/api/history/year/[...year]/+server.ts
+++ src/routes/api/history/year/[...year]/+server.ts
@@ -22,87 +22,93 @@ export const GET = (async ({platform, params, locals, fetch}) => {
 
     const start = Date.now();
 
+    const listPromises = [];
+
     for (const year of years) {
-        let list_complete = false;
-        let cursor: string | undefined = undefined;
-        while(!list_complete) {
-            const listStart = Date.now();
-            const list: KVListResponse<OldShowMeta> = await history.list<OldShowMeta>({
-                prefix: year == "all" ? undefined : year+"",
-                cursor
-            });
+        listPromises.push((async () => {
+            let list_complete = false;
+            let cursor: string | undefined = undefined;
+            while(!list_complete) {
+                const listStart = Date.now();
+                const list: KVListResponse<OldShowMeta> = await history.list<OldShowMeta>({
+                    prefix: year == "all" ? undefined : year+"",
+                    cursor
+                });
 
-            locals.addTiming({
-                id: "list" + lists,
-                description: "List #" + (lists + 1),
-                duration: Date.now() - listStart
-            });
+                locals.addTiming({
+                    id: "list" + lists,
+                    description: "List #" + (lists + 1),
+                    duration: Date.now() - listStart
+                });
 
-            lists++;
+                lists++;
 
-            for(const k of list.keys) {
-                keyNames.push(k.name);
+                for(const k of list.keys) {
+                    keyNames.push(k.name);
+                }
+
+                for (const k of list.keys) {
+                    if(k.name.includes(":")) {
+                        const parts = k.name.split(":");
+                        if(keyNames.includes(parts[0])) continue;
+
+                        keyNames.push(parts[0]);
+                        keyPromises.push((async () => {
+                            const preStart = history.get(parts[0] + ":preShowStart", {cacheTtl});
+                            const mainStart = history.get(parts[0] + ":mainShowStart", {cacheTtl});
+                            const mainEnd = history.get(parts[0] + ":showEnd", {cacheTtl});
+                            const snippet: Promise<YoutubeSnippet | null> = history.get(parts[0] + ":snippet", {cacheTtl, type: "json"});
+                            let isCurrentlyLive: Promise<boolean>;
+                            if(Date.now() - new Date(parts[0]).getTime() < 35 * 60 * 60e3) {
+                                isCurrentlyLive = fetch("/api/twitch?fast=true").then(r => r.json())
+                                  .then(d => !!d.isWAN)
+                            } else {
+                                isCurrentlyLive = Promise.resolve(false);
+                            }
+                            return {
+                                name: parts[0],
+                                metadata: {
+                                    preShowStart: await preStart,
+                                    mainShowStart: await mainStart,
+                                    showEnd: await mainEnd,
+                                    snippet: await snippet,
+                                    title: await (async () => {
+                                        const rawTitle = (await snippet)?.title;
+                                        if(!rawTitle) return rawTitle;
+
+                                        return removeAfterLastDash(rawTitle)
+                                    })(),
+                                    isCurrentlyLive: await isCurrentlyLive
+                                }
+                            }
+                        })());
+                    } else if(!k.metadata) {
+                        keyPromises.push((async () => {
+                            return {
+                                name: k.name,
+                                metadata: (await history.get(k.name, {type: 'json', cacheTtl}) as unknown as OldShowMeta)
+                            }
+                        })());
+                    } else {
+                        keyPromises.push(Promise.resolve(k));
+                    }
+                }
+                list_complete = list.list_complete;
+                cursor = list.cursor;
             }
 
-            for (const k of list.keys) {
-                if(k.name.includes(":")) {
-                    const parts = k.name.split(":");
-                    if(keyNames.includes(parts[0])) continue;
-
-                    keyNames.push(parts[0]);
-                    keyPromises.push((async () => {
-                        const preStart = history.get(parts[0] + ":preShowStart", {cacheTtl});
-                        const mainStart = history.get(parts[0] + ":mainShowStart", {cacheTtl});
-                        const mainEnd = history.get(parts[0] + ":showEnd", {cacheTtl});
-                        const snippet: Promise<YoutubeSnippet | null> = history.get(parts[0] + ":snippet", {cacheTtl, type: "json"});
-                        let isCurrentlyLive: Promise<boolean>;
-                        if(Date.now() - new Date(parts[0]).getTime() < 35 * 60 * 60e3) {
-                            isCurrentlyLive = fetch("/api/twitch?fast=true").then(r => r.json())
-                              .then(d => !!d.isWAN)
-                        } else {
-                            isCurrentlyLive = Promise.resolve(false);
-                        }
-                        return {
-                            name: parts[0],
-                            metadata: {
-                                preShowStart: await preStart,
-                                mainShowStart: await mainStart,
-                                showEnd: await mainEnd,
-                                snippet: await snippet,
-                                title: await (async () => {
-                                    const rawTitle = (await snippet)?.title;
-                                    if(!rawTitle) return rawTitle;
-
-                                    return removeAfterLastDash(rawTitle)
-                                })(),
-                                isCurrentlyLive: await isCurrentlyLive
-                            }
-                        }
-                    })());
-                } else if(!k.metadata) {
-                    keyPromises.push((async () => {
-                        return {
-                            name: k.name,
-                            metadata: (await history.get(k.name, {type: 'json', cacheTtl}) as unknown as OldShowMeta)
-                        }
-                    })());
-                } else {
-                    keyPromises.push(Promise.resolve(k));
+            if(Number(year) <= 2023) {
+                for (const historicalShow of historicalShows) {
+                    if(!historicalShow.name.startsWith(year)) continue;
+                    if(keyNames.includes(historicalShow.name)) continue;
+                    keyNames.push(historicalShow.name);
+                    keyPromises.push(Promise.resolve(historicalShow));
                 }
             }
-            list_complete = list.list_complete;
-            cursor = list.cursor;
-        }
-
-        if(Number(year) <= 2023) {
-            for (const historicalShow of historicalShows) {
-                if(!historicalShow.name.startsWith(year)) continue;
-                if(keyNames.includes(historicalShow.name)) continue;
-                keyNames.push(historicalShow.name);
-                keyPromises.push(Promise.resolve(historicalShow));
-            }
-        }
+        })())
     }
+
+    await Promise.all(listPromises);
 
     const afterLoopStart = Date.now();
 
