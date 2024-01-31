5e7516f53b78a41988b8e41f78c8206b8c08f660
ajgeiss0702 <aj@ajg0702.us>
2024-01-31 11:07:19 -0700

    push it back

--- src/routes/api/specialStream/+server.ts
+++ src/routes/api/specialStream/+server.ts
@@ -12,7 +12,7 @@ export const GET = (async ({fetch}) => {
     title: "Wood PC 2: They said they could do it better, I agree.",
     thumbnail: "https://pbs.floatplane.com/stream_thumbnails/5c13f3c006f1be15e08e05c0/530913290833307_1706722559012.jpeg",
 
-    start: "2024-01-31T18:00:00.000Z",
+    start: "2024-01-31T19:00:00.000Z",
 
     onFloatplane: true,
 
