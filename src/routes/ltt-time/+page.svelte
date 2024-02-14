bc72dabd54f3fae21e838837b62774512405100e
ajgeiss0702 <ajgeiss72@gmail.com>
2024-02-14 13:19:25 -0700

    fade in ltt-time when in frame

--- src/routes/ltt-time/+page.svelte
+++ src/routes/ltt-time/+page.svelte
@@ -2,21 +2,28 @@
   import LTTTime from "$lib/LTTTime.svelte";
   import { browser } from "$app/environment";
   import { page } from "$app/stores";
+  import { onMount } from "svelte";
+  import {fade} from "svelte/transition";
 
   $: isFrame = $page.url.searchParams.has("frame");
 
   $: if(browser && isFrame) {
     document.body.classList.add("background-none")
   }
+
+  let mounted = false;
+  onMount(() => mounted = true)
 </script>
 <svelte:head>
   <title>LTT Time</title>
   <meta name="description" content="What time is it for LTT?">
 </svelte:head>
 
-<div class="flex h-screen items-center justify-center" class:bigtext={!isFrame}>
-  <LTTTime border={isFrame} tooltip={!isFrame}/>
-</div>
+{#if isFrame ? mounted : true}
+  <div class="flex h-screen items-center justify-center" class:bigtext={!isFrame} in:fade>
+    <LTTTime border={isFrame} tooltip={!isFrame}/>
+  </div>
+{/if}
 
 <style>
   .bigtext {
