7148dc4548f5cf5692a6ecb7020444666f5a781b
ajgeiss0702 <ajgeiss72@gmail.com>
2024-02-24 12:01:01 -0700

    it worked, so update this

--- src/routes/api/(live-statuses)/aggregate/+server.ts
+++ src/routes/api/(live-statuses)/aggregate/+server.ts
@@ -88,7 +88,7 @@ export const GET = (async ({url, fetch, locals, platform}) => {
         specialStream: await specialStream,
         floatplane: await floatplane,
         notablePeople: await notable,
-        reloadNumber: 12
+        reloadNumber: 13
         // showExtension: await showExtension
     }
 
