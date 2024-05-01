e9b7d47b27529f844500220177633c97823da567
ajgeiss0702 <ajgeiss72@gmail.com>
2024-05-01 09:56:03 -0700

    only create news db in dev

--- src/routes/news/+page.server.ts
+++ src/routes/news/+page.server.ts
@@ -3,6 +3,7 @@ import type { D1Database } from "@cloudflare/workers-types";
 import { error } from "@sveltejs/kit";
 import { first_news_post, type NewsPost } from "$lib/news/news.ts";
 import { truncateText } from "$lib/utils.ts";
+import { dev } from "$app/environment";
 
 let first = true;
 
@@ -11,8 +12,8 @@ export const load = (async ({platform}) => {
 
   if(!db) throw error(503, "Database missing");
 
-  // only run once per instance. In the future, we'll probably only need to run this on dev
-  if(first) {
+  // only run once per dev instance.
+  if(first && dev) {
     first = false;
     await db.prepare("create table if not exists news (timestamp INTEGER PRIMARY KEY, url TEXT, title TEXT, body TEXT)").run();
     await Promise.all([
