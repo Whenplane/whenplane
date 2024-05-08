d22402795da355847a6c1018fbfa5e54482c6959
ajgeiss0702 <aj@ajg0702.us>
2024-05-08 14:06:46 -0700

    supposed to be 10

--- src/routes/api/specialStream/+server.ts
+++ src/routes/api/specialStream/+server.ts
@@ -10,7 +10,7 @@ export const GET = (async ({fetch}) => {
     title: "I’m Selling This PC for $2… I Hope It Sucks",
     thumbnail: "https://pbs.floatplane.com/stream_thumbnails/5c13f3c006f1be15e08e05c0/096166759055993_1715193799485.jpeg",
 
-    start: "2024-05-08T21:00:00Z",
+    start: "2024-05-08T21:10:00Z",
 
     onFloatplane: true,
 
