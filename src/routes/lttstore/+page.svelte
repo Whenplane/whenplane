66f3bd5cb1b5e5cf96f7695d3253831eb96f2eea
ajgeiss0702 <aj@ajg0702.us>
2024-06-11 15:35:38 -0700

    bit more

--- src/routes/lttstore/+page.svelte
+++ src/routes/lttstore/+page.svelte
@@ -2,8 +2,13 @@
   export let data;
 </script>
 
-{#each data.popularProducts as product}
-  {product.title}
-{:else}
-  No products are being tracked yet!
-{/each}
+<div class="container mx-auto">
+  {#each data.popularProducts as product}
+    <div class="card inline-block p-2">
+      {product.title}<br>
+      {Math.round(Number(product.purchasesPerHour) * 100)/100} purchases per hour
+    </div>
+  {:else}
+    No products are being tracked yet!
+  {/each}
+</div>
