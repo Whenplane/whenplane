41d81cc76b314ffd1ab2adb82e169acca6fa1518
ajgeiss0702 <aj@ajg0702.us>
2024-06-14 13:23:53 -0700

    Add title to all products page

--- src/routes/lttstore/products/+page.svelte
+++ src/routes/lttstore/products/+page.svelte
@@ -8,6 +8,10 @@
   export let data;
 </script>
 
+<svelte:head>
+  <title>All Products - Whenplane LTTStore Watcher</title>
+</svelte:head>
+
 <ol class="breadcrumb pt-2 pl-2">
   <li class="crumb"><a class="anchor hover-underline" href="/">{$page.url.hostname === "whenwan.show" ? "whenwan.show" : "Whenplane"}</a></li>
   <li class="crumb-separator" aria-hidden="true">&rsaquo;</li>
