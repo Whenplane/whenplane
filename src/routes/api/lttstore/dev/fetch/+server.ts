0360069b86d4b3f05e3fcb0c6dcf37ea336bafbb
ajgeiss0702 <aj@ajg0702.us>
2024-06-13 21:24:30 -0700

    darnit, i did it again

--- src/routes/api/lttstore/dev/fetch/+server.ts
+++ src/routes/api/lttstore/dev/fetch/+server.ts
@@ -20,7 +20,7 @@ export const GET = (async ({platform, params}) => {
   let i = 0;
   for (const product of data.products) {
     console.log("Inserting (" + ++i + "/" + data.products.length + ") " + product.title);
-    await db.prepare("insert or replace into products(handle, id, title, product, stock, stockChecked, lastRestock, purchasesPerHour, regularPrice, currentPrice, firstSeen, available) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+    await db.prepare("insert or replace into products(handle, id, title, product, stock, stockChecked, lastRestock, purchasesPerHour, regularPrice, currentPrice, firstSeen, available) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
       .bind(
         product.handle,
         product.id,
