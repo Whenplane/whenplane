57d500ae902f6ca0cdb590d44d6b8311e43277b1
ajgeiss0702 <aj@ajg0702.us>
2024-07-03 10:39:25 -0700

    add currency conversion warning

--- src/routes/lttstore/+layout.svelte
+++ src/routes/lttstore/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
   import { setCookie } from "$lib/cookieUtils";
   import { invalidateAll } from "$app/navigation";
+  import ExclamationTriangle from "svelte-bootstrap-icons/lib/ExclamationTriangle.svelte";
+  import {popup} from "@skeletonlabs/skeleton";
+  import {fade} from "svelte/transition"
 
   export let data;
 
@@ -12,14 +15,36 @@
 
 </script>
 <div class="float-right pr-5 h-0">
-  <select class="select w-20 p-1 pl-2 mt-8 sm:mt-1" bind:value={selectedCurrency}>
-    {#each Object.entries(data.exchangeRates.rates) as [code, exchange] (code)}
-      <option>{code}</option>
-    {/each}
-  </select>
+  <div class="inline-block">
+    {#if selectedCurrency !== "USD"}
+      <span class="[&>*]:pointer-events-none" use:popup={{
+        event: "hover",
+        target: "currency-warning",
+        placement: 'bottom'
+      }} transition:fade={{duration: 150}}>
+        <ExclamationTriangle class="inline-block text-yellow-300 h-7 w-7"/>
+      </span>
+    {/if}
+    <select class="select w-20 p-1 pl-2 mt-8 sm:mt-1" bind:value={selectedCurrency}>
+      {#each Object.entries(data.exchangeRates.rates) as [code, exchange] (code)}
+        <option>{code}</option>
+      {/each}
+    </select>
+  </div>
 </div>
 <slot/>
 <br>
 <div class="p-2">
   Images and products from <a href="https://lttstore.com/?ref=whenplane.com" class="anchor">lttstore.com</a><br>
+</div>
+<div class="card py-2 px-4" data-popup="currency-warning">
+  <p>
+    This currency conversion is only an estimate.<br>
+    Conversion rates are only updated once per day,<br>
+    and will probably <b>not</b> reflect the exact rate<br>
+    used to charge you if you were to purchase.<br>
+    <br>
+    LTTStore charges only in USD.
+  </p>
+  <div class="arrow variant-filled-surface" />
 </div>
