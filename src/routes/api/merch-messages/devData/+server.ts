8cce68e8ca95adc6e725f2ecbd88615280c17f33
ajgeiss0702 <aj@ajg0702.us>
2024-07-15 22:34:14 -0500

    whoops

--- src/routes/api/merch-messages/devData/+server.ts
+++ src/routes/api/merch-messages/devData/+server.ts
@@ -6,7 +6,7 @@ export const GET = (async ({platform, params}) => {
   const db: D1Database | undefined = platform?.env?.MERCHMESSAGES_DB;
   if(!db) throw error(503, "DB unavailable!");
 
-  const merchMessages = db.prepare("select * from merch_messages")
+  const merchMessages = await db.prepare("select * from merch_messages")
     .all()
     .then(r => r.results);
 
