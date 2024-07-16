4215f63fd21add0c678ccdbd62f748bf57b92a14
ajgeiss0702 <aj@ajg0702.us>
2024-07-16 13:58:45 -0500

    get videos table for devdata

--- src/routes/api/merch-messages/devData/+server.ts
+++ src/routes/api/merch-messages/devData/+server.ts
@@ -6,10 +6,14 @@ export const GET = (async ({platform, params}) => {
   const db: D1Database | undefined = platform?.env?.MERCHMESSAGES_DB;
   if(!db) throw error(503, "DB unavailable!");
 
+  const videos = await db.prepare("select * from videos")
+    .all()
+    .then(r => r.results);
+
   const merchMessages = await db.prepare("select * from merch_messages")
     .all()
     .then(r => r.results);
 
-  return json({merchMessages})
+  return json({videos, merchMessages})
 
 }) satisfies RequestHandler
