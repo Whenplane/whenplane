0c31ebd5fe387750f313aa2342a8f6c36ab505a6
ajgeiss0702 <aj@ajg0702.us>
2024-07-29 13:03:57 -0500

    update thumbnail

--- src/routes/api/specialStream/+server.ts
+++ src/routes/api/specialStream/+server.ts
@@ -8,8 +8,8 @@ export const GET = (async ({fetch}) => {
   // In the future this will be from a database, but this will be fine for now.
 
   const data: SpecialStream | false = {
-    title: "FPX: Painting with 'AI Thermal Colored Pastes'",
-    thumbnail: "https://images.ajg0702.us/painting_stream_preview.jpeg",
+    title: "FPX: Painting with 'AI Thermal Colored Pastes')",
+    thumbnail: "https://pbs.floatplane.com/stream_thumbnails/5c13f3c006f1be15e08e05c0/870001146186448_1722275993944.jpeg",
 
     start: "2024-07-29T18:00:00Z",
     startIsEstimated: false,
