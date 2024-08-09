2aad28885ae34c1578368e3eda2631baa593fd1d
ajgeiss0702 <aj@ajg0702.us>
2024-08-08 18:35:47 -0700

    confetti for end of subathon

--- src/routes/(fun)/boca-subathon/+page.svelte
+++ src/routes/(fun)/boca-subathon/+page.svelte
@@ -2,15 +2,20 @@
   import { onMount } from "svelte";
   import {timeStringHours, timeString} from "$lib/timeUtils";
   import {ProgressBar} from "@skeletonlabs/skeleton";
+  import Confetti from "svelte-confetti"
+  import { dev } from "$app/environment";
 
-  const startTime = 1722647766000;
   const totalTime = (150 * 60 * 60e3);
+  // this is multiple lines, so I can easily comment/uncomment
+  const startTime =
+    dev ? Date.now() - totalTime + 2e3 :
+      1722647766000;
 
   let string: string | undefined = "";
   let distance: number;
 
   function update() {
-    distance = Date.now() - startTime;
+    distance = Math.min(Date.now() - startTime, totalTime);
     string = timeStringHours(distance);
   }
 
@@ -26,7 +31,7 @@
   <meta name="description" content="Boca subathon progress % and time remaining">
 </svelte:head>
 
-<div class="limit mx-auto pt-8">
+<div class="limit mx-auto pt-8 p-2">
   <h1>{string}</h1>
   into the subathon.
   <br>
@@ -42,12 +47,31 @@
   <br>
   There is
   <span class="font-mono">
-    {timeString(totalTime - distance, true)}
+    {timeString(totalTime - distance, true)?.trim()}
   </span>
   remaining.<br>
-  aka
-  <span class="font-mono">
-    {timeStringHours(totalTime - distance)}
-  </span>
+  {#if totalTime - distance > 24 * 60 * 60e3}
+    aka
+    <span class="font-mono">
+      {timeStringHours(totalTime - distance)}
+    </span>
+  {/if}
 
+  <div class="confetti-container">
+    {#if distance >= totalTime}
+      <Confetti delay={[0, 2000]} cone x={[-1.5, 1.5]} y={[0.25, 1.5]} amount={50} fallDistance="100px" iterationCount=infinite/>
+      <Confetti delay={[0, 2000]} x={[-2.5, 2.5]} y={[0.75, 2.25]} amount={100} iterationCount=infinite/>
+    {/if}
+  </div>
 </div>
+
+<style>
+  .confetti-container {
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      /*margin-left: -50px;*/
+      width: 5px;
+      height: 5px;
+  }
+</style>
