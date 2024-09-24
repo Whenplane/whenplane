2ea8146891228742bc0f86801dd342a006a379be
ajgeiss0702 <aj@ajg0702.us>
2024-09-24 11:51:40 -0700

    Add some more field names

--- src/lib/lttstore/field_names.ts
+++ src/lib/lttstore/field_names.ts
@@ -1,7 +1,12 @@
 import TextDiff from "$lib/lttstore/diff/TextDiff.svelte";
 
 const field_names: {[key: string]: string} = {
-  "product.description": "Product Description"
+  "product.description": "Product Description",
+  "product.available": "In stock?",
+  "currentPrice": "Current Price",
+  "regularPrice": "Regular Price",
+  "title": "Title",
+  "backorderAlerts": "Backorder Alerts"
 }
 
 export function getFieldName(key: string) {
