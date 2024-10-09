31fdcad06b933f3d8632a67ec3485262cf7c1822
ajgeiss0702 <aj@ajg0702.us>
2024-10-09 08:29:03 -0700

    add change history endpoint

---
+++ src/routes/api/lttstore/products/[handle]/changeHistory/+server.ts
@@ -0,0 +1,24 @@
+import { error, json } from "@sveltejs/kit";
+import type { D1Database } from "@cloudflare/workers-types";
+import { dev } from "$app/environment";
+import { createTables } from "../../../../../lttstore/createTables.ts";
+
+
+export const GET = (async ({fetch, params, platform}) => {
+  const db: D1Database | undefined = platform?.env?.LTTSTORE_DB;
+  if(!db) throw error(503, "DB unavailable!");
+
+  if(dev) await createTables(db);
+
+  const brief: {id: number, handle: string, title: string} | undefined = await fetch(`/api/lttstore/products/brief/${params.handle}`)
+    .then(r => r.ok ? r.json() : undefined);
+
+  if(!brief) throw error(404, 'Product not found (this could also be an internal error)');
+
+  const changeHistory = await db.prepare("select * from change_history where id = ? order by timestamp desc")
+    .bind(brief.id)
+    .all<{id: number, timestamp: number, field: string, old: string, new: string}>()
+    .then(r => r.results);
+
+  return json(changeHistory);
+})
