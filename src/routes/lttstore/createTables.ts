16df002c9b6c63a28ff8bdb07e03f0258004fcb0
ajgeiss0702 <aj@ajg0702.us>
2024-10-08 23:13:20 -0700

    ah, heres why i couldnt reproduce it locally because changes from the same time and product wouldnt be copied lol

--- src/routes/lttstore/createTables.ts
+++ src/routes/lttstore/createTables.ts
@@ -5,6 +5,6 @@ export async function createTables(db: D1Database) {
     .run();
   await db.prepare("create table if not exists stock_history (handle text, id integer, timestamp integer, stock string, unique(id, timestamp))")
     .run();
-  await db.prepare("create table if not exists change_history (id integer, timestamp integer, field TEXT, old TEXT, new TEXT, unique(id, timestamp))")
+  await db.prepare("create table if not exists change_history (id integer, timestamp integer, field TEXT, old TEXT, new TEXT, unique(id, timestamp, field))")
     .run();
 }
