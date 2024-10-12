194926a95c8ea5918fa2e59f884ee40ba48090e1
ajgeiss0702 <aj@ajg0702.us>
2024-10-12 12:24:39 -0700

    oh lol i pushed, then popped so i removed the one i pushed

--- src/routes/(fun)/boca-marathon/+page.server.ts
+++ src/routes/(fun)/boca-marathon/+page.server.ts
@@ -74,8 +74,8 @@ export const load = (async ({platform, fetch}) => {
 
         if(length < 15e3) remove.push(thisEvent);
       }
-      remove.push(pastData[0]); // add first element, as it usually wouldnt meet above requirements and is the furthest off
       remove.pop() // remove the last (oldest) element, since that should be the most accurate
+      remove.push(pastData[0]); // add first element, as it usually wouldnt meet above requirements and is the furthest off
 
       console.log("I would remove", remove)
 
