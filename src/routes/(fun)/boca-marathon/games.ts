fd04ed1d95b80f07cfc4be42ee44f45ab0a70a5b
ajgeiss0702 <aj@ajg0702.us>
2024-10-12 17:15:55 -0700

    update games

--- src/routes/(fun)/boca-marathon/games.ts
+++ src/routes/(fun)/boca-marathon/games.ts
@@ -3,11 +3,11 @@ export const games: {[game: string]: string} = {
   "Left 4 Dead 2": "",
   "Halo 3": "",
   "Fall Guys": "",
-  "Mario Party": "Boca has not said yet which mario party they will play. He did say one of the GameCube ones though",
-  "Minecraft": "",
+  "Mario Party 6": "",
+  "Minecraft": "Either normal or hard (probably normal)",
   "Portal 2": "",
   "Bread & Fred": "",
   "Keep Talking and Nobody Explodes": "",
   "A Way Out": "",
-  "Streamers vs Chatters": "Near the end of the stream, the streamers will need to beat chat in a game. They cannot end stream until they beat chat."
+  "Streamers vs Chatters": "Near the end of the stream, the streamers will need to beat chat in a game. They cannot end stream until they beat chat. Will probably be Higher or Lower."
 }
