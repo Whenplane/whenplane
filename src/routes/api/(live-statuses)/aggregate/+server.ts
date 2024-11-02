8974719e79501b200cd3fb746bfb72b872d896f2
ajgeiss0702 <aj@ajg0702.us>
2024-11-01 17:00:51 -0700

    bump

--- src/routes/api/(live-statuses)/aggregate/+server.ts
+++ src/routes/api/(live-statuses)/aggregate/+server.ts
@@ -97,7 +97,7 @@ export const GET = (async ({url, fetch, locals, platform}) => {
         specialStream: await specialStream,
         floatplane: await floatplane,
         notablePeople: await notable,
-        reloadNumber: 36
+        reloadNumber: 37
         // showExtension: await showExtension
     }
 
