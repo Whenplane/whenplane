3d42e9753224968978e207577f878bca798b07f8
ajgeiss0702 <aj@ajg0702.us>
2024-11-15 14:03:43 -0700

    Change date format in search results

--- src/lib/history/MiniShow.svelte
+++ src/lib/history/MiniShow.svelte
@@ -9,7 +9,7 @@
   <img src="{show.metadata?.thumbnails?.maxres?.url ?? 'https://i.ytimg.com/vi/' + show.metadata.vods?.youtube + '/maxresdefault.jpg'}" alt="" aria-hidden="true" loading={i > 10 ? "lazy" : undefined}>
   <div class="p-2">
     {show.metadata.title}<br>
-    {new Date(show.name).toLocaleDateString()}
+    {new Date(show.name).toLocaleDateString(undefined, {dateStyle: "long"})}
   </div>
 </div>
 <style>
