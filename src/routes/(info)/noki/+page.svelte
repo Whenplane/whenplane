127888955356d8ab4bee6266841b881a8e1f5116
ajgeiss0702 <aj@ajg0702.us>
2024-11-15 14:20:16 -0700

    add noki page

---
+++ src/routes/(info)/noki/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+</script>
+
+<ol class="breadcrumb pt-2 pl-2">
+  <li class="crumb"><a class="anchor hover-underline" href="/">{$page.url.hostname === "whenwan.show" ? "whenwan.show" : "Whenplane"}</a></li>
+  <li class="crumb-separator" aria-hidden="true">&rsaquo;</li>
+  <li class="crumb">Noki</li>
+</ol>
+
+<div class="p-2 limit mx-auto">
+  <div class="flex items-center p-2">
+    <img src="/profiles/noki.jpg" alt="" aria-hidden="true" class="placeholder-circle">
+    <h1 class="p-4">NoKi1119</h1>
+  </div>
+  <b>HUGE</b> thank you to the one and only NoKi for providing timestamps/topics that are displayed throughout Whenplane.<br>
+  <br>
+  Here are his links:<br>
+  <a href="https://www.youtube.com/@NoKi1119">YouTube @NoKi1119</a><br>
+  <a href="https://ko-fi.com/noki1119">Ko-Fi (Donations)</a><br>
+  <a href="https://docs.google.com/document/d/1R8f1IILzJV-xH6LP7Npj5PNgrI8DquxicFjZOxJvQgI/edit?usp=sharing">Timestamps Doc</a><br>
+</div>
