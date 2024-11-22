b7a62bcc7d8ec019d3266697be7337c6e3bf1378
ajgeiss0702 <aj@ajg0702.us>
2024-11-21 20:10:25 -0700

    yeah should do this too

--- src/routes/api/(live-statuses)/aggregate/+server.ts
+++ src/routes/api/(live-statuses)/aggregate/+server.ts
@@ -97,7 +97,7 @@ export const GET = (async ({url, fetch, locals, platform}) => {
         specialStream: await specialStream,
         floatplane: await floatplane,
         notablePeople: await notable,
-        reloadNumber: 38
+        reloadNumber: 39
         // showExtension: await showExtension
     }
 
