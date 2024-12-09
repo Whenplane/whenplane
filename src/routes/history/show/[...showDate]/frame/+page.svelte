612e36b061dc0a0e77997cdf80b2ec8212f1a3af
ajgeiss0702 <aj@ajg0702.us>
2024-12-09 15:50:23 -0700

    add show info frame page

---
+++ src/routes/history/show/[...showDate]/frame/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import HistoricalShow from "$lib/history/HistoricalShow.svelte";
+
+  export let data;
+</script>
+
+<div class="text-center">
+  <HistoricalShow onlyTimes={true} show={data}/>
+</div>
