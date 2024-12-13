4cd76564e569e6555c999182bdda8fadce493e19
ajgeiss0702 <aj@ajg0702.us>
2024-12-13 12:25:19 -0700

    also log error because maybe its not json serializable for some reason?

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -179,7 +179,7 @@ export const handle: Handle = async ({ event, resolve }) => {
 export const handleError: HandleServerError = async ({ error, event, status, message}) => {
 
     if(!building && error && error?.status !== 404) {
-        console.debug("Reporting error!")
+        console.debug("Reporting error!", error)
         if(env.ERROR_REPORTING_WEBHOOK) {
 
             try {
