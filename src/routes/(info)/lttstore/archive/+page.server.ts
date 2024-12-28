63dc1fc1f4597739271ad72343a3607f0be7843e
ajgeiss0702 <aj@ajg0702.us>
2024-12-28 13:07:45 -0700

    add archive page

---
+++ src/routes/(info)/lttstore/archive/+page.server.ts
@@ -0,0 +1,16 @@
+import { error, type ServerLoad } from "@sveltejs/kit";
+import type { ProductsTableRow } from "$lib/lttstore/lttstore_types.ts";
+import type { D1Database } from "@cloudflare/workers-types";
+
+
+export const load = (async ({platform}) => {
+
+  const db: D1Database | undefined = platform?.env?.LTTSTORE_DB;
+  if(!db) throw error(503, "DB unavailable!");
+
+  const products = await db.prepare("select title,handle,product from products where available=0 order by json_extract(product, '$.created_at') ASC")
+    .all<ProductsTableRow>()
+    .then(r => r.results);
+
+  return {products}
+}) satisfies ServerLoad
---
+++ src/routes/(info)/lttstore/archive/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import LTTProductCard from "$lib/lttstore/LTTProductCard.svelte";
+  import { page } from "$app/stores";
+
+  export let data;
+</script>
+<svelte:head>
+  <title>LTTStore Product Archive - Whenplane</title>
+  <meta name="description" content="These products are no longer on lttstore. They are preserved here for archival purposes."/>
+</svelte:head>
+
+<ol class="breadcrumb pt-2 pl-2">
+  <li class="crumb"><a class="anchor hover-underline" href="/">{$page.url.hostname === "whenwan.show" ? "whenwan.show" : "Whenplane"}</a></li>
+  <li class="crumb-separator" aria-hidden="true">&rsaquo;</li>
+  <li class="crumb"><a class="anchor hover-underline" href="/lttstore">LTT Store Tracker</a></li>
+  <li class="crumb-separator" aria-hidden="true">&rsaquo;</li>
+  <li class="crumb">Product Archive</li>
+</ol>
+
+<div class="p-2 mt-8 mb-32">
+  <div class="p-2 limit mx-auto">
+    <h1>LTTStore Product Archive</h1>
+    <br>
+    These products are no longer on lttstore. They are preserved here for archival purposes.
+  </div>
+  <br>
+  <div class="p-2">
+    {#each data.products as product}
+      <LTTProductCard product={JSON.parse(product.product)}/>
+    {/each}
+  </div>
+</div>
