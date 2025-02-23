73efac2dbd6f55d65437717601a94ffa1c24e117
ajgeiss0702 <aj@ajg0702.us>
2025-02-22 17:07:29 -0700

    support png for qr code

--- src/routes/api/(other)/qrcode/+server.ts
+++ src/routes/api/(other)/qrcode/+server.ts
@@ -14,12 +14,36 @@ export const GET = (async ({url, platform}) => {
     if(!tempText) throw error(404, "ID not found");
   }
   const text = url.searchParams.get("text") ?? tempText ?? "https://whenplane.com"
-  const image = await QRCode.toString(text, {type: "svg"});
 
-  return new Response(image, {
-    headers: {
-      "content-type": "image/svg+xml",
-      "content-length": image.length+""
-    }
-  });
+  const type = url.searchParams.has("png") ? "png" : "svg";
+
+  if(type === "svg") {
+    const image = await QRCode.toString(text, {type: "svg"});
+
+    return new Response(image, {
+      headers: {
+        "content-type": "image/svg+xml",
+        "content-length": image.length+""
+      }
+    });
+  } else {
+    const image = await QRCode.toDataURL(text, {type: "image/png", scale: 10, margin: 2});
+    const response = base64ToArrayBuffer(image.split("base64,")[1]);
+
+    return new Response(response, {
+      headers: {
+        "content-type": "image/png",
+        "content-length": response.byteLength+""
+      }
+    });
+  }
 }) satisfies RequestHandler;
+
+function base64ToArrayBuffer(base64: string) {
+  const binaryString = atob(base64);
+  const bytes = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes.buffer;
+}
