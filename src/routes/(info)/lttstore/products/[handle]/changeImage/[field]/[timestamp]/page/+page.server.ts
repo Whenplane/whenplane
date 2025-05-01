3f82901d3594988ad97006ae9273397cebf935ee
ajgeiss0702 <aj@ajg0702.us>
2025-04-30 21:10:45 -0700

    add page for screenshotting a change

---
+++ src/routes/(info)/lttstore/products/[handle]/changeImage/[field]/[timestamp]/page/+page.server.ts
@@ -0,0 +1,27 @@
+import type { PageServerLoad } from "./$types";
+import { error } from "@sveltejs/kit";
+
+export const load = (async ({params, platform, url}) => {
+  const productId = Number(params.handle ?? "NaN");
+  const field = params.field;
+  const timestamp = Number(params.timestamp ?? "NaN");
+
+  if(isNaN(timestamp) || isNaN(productId)) throw error(400);
+
+  const bookmark = url.searchParams.get("bookmark");
+
+  const db = platform?.env?.LTTSTORE_DB.withSession(bookmark ?? "first-primary");
+  if(!db) throw error(503, "Missing db!");
+
+  const change = await db.prepare("select * from change_history where id = ? and timestamp = ? and field = ?")
+    .bind(productId, timestamp, field)
+    .first<{id: number, timestamp: number, field: string, old: string, new: string}>();
+
+  if(!change) throw error(404);
+
+  return {
+    change,
+  }
+
+
+}) satisfies PageServerLoad
---
+++ src/routes/(info)/lttstore/products/[handle]/changeImage/[field]/[timestamp]/page/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { getFieldName } from "$lib/lttstore/field_names";
+  import { getDiffComponent } from "$lib/lttstore/field_components";
+  import DateStamp from "$lib/DateStamp.svelte";
+
+  export let data;
+</script>
+
+<div class="table-container rounded-md" id="change">
+  <table class="table table-hover rounded-md">
+    <thead>
+    <tr>
+      <th>What changed</th>
+      <th>Before</th>
+      <th>After</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr id="change">
+      <td>{getFieldName(data.change.field)}</td>
+      <td><svelte:component this={getDiffComponent(data.change.field)} before={data.change.old} after={data.change.new} displaying="before"/></td>
+      <td><svelte:component this={getDiffComponent(data.change.field)} before={data.change.old} after={data.change.new} displaying="after"/></td>
+    </tr>
+    </tbody>
+  </table>
+</div>
+
+<style>
+  #change {
+      width: fit-content;
+      max-width: 90vw;
+  }
+</style>
