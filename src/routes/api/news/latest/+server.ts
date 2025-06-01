3a9c7bac6960656d6418df8d0a94d108792537c6
ajgeiss0702 <aj@ajg0702.us>
2025-06-01 14:50:53 -0700

    clean up code for api/news/latest

--- src/routes/api/news/latest/+server.ts
+++ src/routes/api/news/latest/+server.ts
@@ -1,6 +1,6 @@
 import { error, json, type RequestHandler } from "@sveltejs/kit";
 import type { NewsPost } from "$lib/news/news.ts";
-import { truncateText } from "$lib/utils.ts";
+import { retryD1, truncateText } from "$lib/utils.ts";
 import { env } from "$env/dynamic/private";
 
 export const GET: RequestHandler = (async ({platform}) => {
@@ -8,22 +8,13 @@ export const GET: RequestHandler = (async ({platform}) => {
   const db = platform?.env?.DB?.withSession();
   if(!db) throw error(503, "Database missing");
 
-  async function tryDB(attempt = 1): Promise<NewsPost>  {
-    if(!db) throw error(503, "Database unavailable!");
-    return await db.prepare("select * from news where timestamp <= ? order by timestamp DESC limit 1")
+  const response = await retryD1(() =>
+    db.prepare("select * from news where timestamp <= ? order by timestamp DESC limit 1")
       .bind(Date.now() + (60 * 60e3))
       .all<NewsPost>()
       .then(r => r.results[0] as unknown as NewsPost)
       .then(n => {return {...n, body: truncateText(n.body, 500)}})
-      .catch(e => {
-        console.warn("Attempt number " + attempt + " failed." + (attempt < 3 ? " Retrying." : ""));
-        if(attempt < 3) {
-          return tryDB(++attempt);
-        } else {
-          throw e;
-        }
-      })
-  }
+  )
 
-  return json(await tryDB());
+  return json(response);
 }) satisfies RequestHandler;
