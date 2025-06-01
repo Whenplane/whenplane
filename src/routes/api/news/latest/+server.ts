a4ec878b84cfa8da6144a9041de239d08093c336
ajgeiss0702 <aj@ajg0702.us>
2025-06-01 15:06:25 -0700

    add short dc cache to latest news

--- src/routes/api/news/latest/+server.ts
+++ src/routes/api/news/latest/+server.ts
@@ -1,12 +1,28 @@
 import { error, json, type RequestHandler } from "@sveltejs/kit";
 import type { NewsPost } from "$lib/news/news.ts";
-import { retryD1, truncateText } from "$lib/utils.ts";
-import { env } from "$env/dynamic/private";
+import { newResponse, retryD1, truncateText } from "$lib/utils.ts";
 
 export const GET: RequestHandler = (async ({platform}) => {
 
   const db = platform?.env?.DB?.withSession();
   if(!db) throw error(503, "Database missing");
+
+  let cCache: Cache | undefined = undefined;
+  let cacheRequest: Request | undefined = undefined;
+  if(typeof caches !== "undefined") {
+    cCache = await caches.open("whenplane:newsCache");
+    cacheRequest = new Request("https://newsCache/latest");
+    const cacheMatch = await cCache.match(cacheRequest);
+
+    if(cacheMatch) {
+      const fetched = cacheMatch.headers.get("x-fetched")
+      if(fetched && Date.now() - new Date(fetched).getTime() < (5 * 60e3)) {
+        return newResponse(cacheMatch);
+      }
+    }
+  } else {
+    console.warn("missing cache api!")
+  }
 
   const response = await retryD1(() =>
     db.prepare("select * from news where timestamp <= ? order by timestamp DESC limit 1")
@@ -15,6 +31,8 @@ export const GET: RequestHandler = (async ({platform}) => {
       .then(r => r.results[0] as unknown as NewsPost)
       .then(n => {return {...n, body: truncateText(n.body, 500)}})
   )
+
+  if(cCache && cacheRequest) platform?.context?.waitUntil(cCache.put(cacheRequest, json(response, {headers: {"x-fetched": new Date().toISOString()}})));
 
   return json(response);
 }) satisfies RequestHandler;
