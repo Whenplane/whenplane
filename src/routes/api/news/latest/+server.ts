3507263253afd97f7ebb2a000c659ed349f0fe93
ajgeiss0702 <aj@ajg0702.us>
2025-06-01 15:14:17 -0700

    allow /api/news/latest to update cache in the background

--- src/routes/api/news/latest/+server.ts
+++ src/routes/api/news/latest/+server.ts
@@ -2,10 +2,32 @@ import { error, json, type RequestHandler } from "@sveltejs/kit";
 import type { NewsPost } from "$lib/news/news.ts";
 import { newResponse, retryD1, truncateText } from "$lib/utils.ts";
 
+let lastBackgroundUpdate = 0;
+
 export const GET: RequestHandler = (async ({platform}) => {
 
   const db = platform?.env?.DB?.withSession();
   if(!db) throw error(503, "Database missing");
+
+  async function doFetch(background = false) {
+    if(!db) throw error(503, "Database missing");
+    const response = await retryD1(() =>
+      db.prepare("select * from news where timestamp <= ? order by timestamp DESC limit 1")
+        .bind(Date.now() + (60 * 60e3))
+        .all<NewsPost>()
+        .then(r => r.results[0] as unknown as NewsPost)
+        .then(n => {return {...n, body: truncateText(n.body, 500)}})
+    )
+    if(cCache && cacheRequest) {
+      const cachePromise = cCache.put(cacheRequest, json(response, {headers: {"x-fetched": new Date().toISOString()}}));
+      if(background) {
+        await cachePromise;
+      } else {
+        platform?.context?.waitUntil(cachePromise);
+      }
+    }
+    return response;
+  }
 
   let cCache: Cache | undefined = undefined;
   let cacheRequest: Request | undefined = undefined;
@@ -16,7 +38,14 @@ export const GET: RequestHandler = (async ({platform}) => {
 
     if(cacheMatch) {
       const fetched = cacheMatch.headers.get("x-fetched")
-      if(fetched && Date.now() - new Date(fetched).getTime() < (5 * 60e3)) {
+      const distance = Date.now() - new Date(fetched ?? 0).getTime()
+      if(distance < (5 * 60e3)) {
+        return newResponse(cacheMatch);
+      } else if(distance < (60 * 60e3)) {
+        if(Date.now() - lastBackgroundUpdate > 60e3) {
+          lastBackgroundUpdate = Date.now();
+          platform?.context?.waitUntil(doFetch(true));
+        }
         return newResponse(cacheMatch);
       }
     }
@@ -24,15 +53,6 @@ export const GET: RequestHandler = (async ({platform}) => {
     console.warn("missing cache api!")
   }
 
-  const response = await retryD1(() =>
-    db.prepare("select * from news where timestamp <= ? order by timestamp DESC limit 1")
-      .bind(Date.now() + (60 * 60e3))
-      .all<NewsPost>()
-      .then(r => r.results[0] as unknown as NewsPost)
-      .then(n => {return {...n, body: truncateText(n.body, 500)}})
-  )
-
-  if(cCache && cacheRequest) platform?.context?.waitUntil(cCache.put(cacheRequest, json(response, {headers: {"x-fetched": new Date().toISOString()}})));
-
+  const response = await doFetch();
   return json(response);
 }) satisfies RequestHandler;
