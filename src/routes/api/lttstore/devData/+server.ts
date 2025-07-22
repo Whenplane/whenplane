88e8c4e7c30ed4551fe3b9bba075e5f55d76347d
ajgeiss0702 <aj@ajg0702.us>
2025-07-22 12:47:09 -0700

    log when first 3 finish

--- src/routes/api/lttstore/devData/+server.ts
+++ src/routes/api/lttstore/devData/+server.ts
@@ -23,6 +23,7 @@ export const GET = (async ({platform}) => {
     .then(r => r.results);
 
   await Promise.all([products, collections, collectionChanges]); // only run 3 at a time
+  console.log("first 3 done")
 
   const screwdriverStocks = db.prepare("select * from stock_history where id = 6649895092327")
     .all()
