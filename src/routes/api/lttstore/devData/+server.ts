7c839a3ddf0187e98238cbd0e13a009929915543
ajgeiss0702 <aj@ajg0702.us>
2025-07-22 12:42:49 -0700

    try this

--- src/routes/api/lttstore/devData/+server.ts
+++ src/routes/api/lttstore/devData/+server.ts
@@ -22,6 +22,8 @@ export const GET = (async ({platform}) => {
     .all()
     .then(r => r.results);
 
+  await Promise.all([products, collections, collectionChanges]); // only run 3 at a time
+
   const screwdriverStocks = db.prepare("select * from stock_history where id = 6649895092327")
     .all()
     .then(r => r.results);
