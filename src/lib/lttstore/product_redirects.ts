ab3eebd5bcd5516d2758f8e546954ba844bc9c5b
ajgeiss0702 <aj@ajg0702.us>
2025-09-17 08:59:30 -0700

    pen redirect

--- src/lib/lttstore/product_redirects.ts
+++ src/lib/lttstore/product_redirects.ts
@@ -4,5 +4,6 @@ export const productRedirects: {[k: string]: string} = {
   "2023-sticker-pack-1": "sticker-pack-set-2",
   "2023-sticker-pack": "sticker-pack-set-2",
   "2022-sticker-pack": "2022-sticker-pack-1",
-  "tall-blank-shirt": "blank-t-shirt-short-sleeve"
+  "tall-blank-shirt": "blank-t-shirt-short-sleeve",
+  "pen": "scribedriver-bolt-action-pen"
 }
