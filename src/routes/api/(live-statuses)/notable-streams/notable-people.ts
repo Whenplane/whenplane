60bb0cb27cde49d01867f24dae79fd49a12430ae
ajgeiss0702 <aj@ajg0702.us>
2025-10-05 19:34:27 -0700

    sammy

--- src/routes/api/(live-statuses)/notable-streams/notable-people.ts
+++ src/routes/api/(live-statuses)/notable-streams/notable-people.ts
@@ -2,5 +2,6 @@ export const notablePeople: { [channel: string]: string } = {
   "bocabola": "Elijah",
   "buhdan": "Dan",
   "luke_lafr": "Luke",
+  "iitskasino": "Sammy"
   // "ajgeiss0702": "Test"
 }
