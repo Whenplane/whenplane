461a66cd6cbbfc648c827f598f6ae707511ecfef
ajgeiss0702 <aj@ajg0702.us>
2025-11-14 12:11:53 -0700

    support post in headers endpoint

--- src/routes/api/test/headers/+server.ts
+++ src/routes/api/test/headers/+server.ts
@@ -1,8 +1,11 @@
 import { json, type RequestHandler } from "@sveltejs/kit";
 
-export const GET = (({request}) => {
+const handle = (({request}) => {
 
 
   return json(Object.fromEntries(request.headers as unknown as Iterable<[string, string]>))
 
-}) satisfies RequestHandler
+}) satisfies RequestHandler;
+
+export const GET = handle;
+export const POST = handle;
