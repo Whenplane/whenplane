419d3087893b046ed3b31361bb64ac6cf05bf203
ajgeiss0702 <aj@ajg0702.us>
2025-12-13 10:32:34 -0700

    sammy changed name

--- src/routes/api/(live-statuses)/notable-streams/notable-people.ts
+++ src/routes/api/(live-statuses)/notable-streams/notable-people.ts
@@ -2,6 +2,6 @@ export const notablePeople: { [channel: string]: string } = {
   "bocabola": "Elijah",
   "buhdan": "Dan",
   "luke_lafr": "Luke",
-  "iitskasino": "Sammy"
+  "imkasino_": "Sammy"
   // "ajgeiss0702": "Test"
 }
