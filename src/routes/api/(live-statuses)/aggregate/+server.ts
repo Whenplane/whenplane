5c832c20baa0b68de4fa1567d8bdc96a73ed0e99
ajgeiss0702 <aj@ajg0702.us>
2025-12-18 14:25:53 -0700

    oh and reload

--- src/routes/api/(live-statuses)/aggregate/+server.ts
+++ src/routes/api/(live-statuses)/aggregate/+server.ts
@@ -108,7 +108,7 @@ export const GET = (async ({url, fetch, locals, platform}) => {
         specialStream: await specialStream,
         floatplane: await floatplane,
         notablePeople: await notable,
-        reloadNumber: 60
+        reloadNumber: 61
         // showExtension: await showExtension
     }
 
