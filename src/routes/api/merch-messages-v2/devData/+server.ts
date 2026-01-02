1b9b394bab4010711562c709b874734b45b567b3
ajgeiss0702 <aj@ajg0702.us>
2026-01-02 13:57:18 -0700

    ah

--- src/routes/api/merch-messages-v2/devData/+server.ts
+++ src/routes/api/merch-messages-v2/devData/+server.ts
@@ -11,7 +11,10 @@ export const GET = (async ({platform}) => {
 
   const latest5 = shows.slice(0, 5).map(v => v.showId);
 
-  const merchMessages = await db.prepare("select * from merch_messages_v2 where show = ? or show = ? or show = ? or show = ? or show = ?")
+  const showParams: string[] = [];
+  latest5.forEach(() => showParams.push("show = ?"));
+
+  const merchMessages = await db.prepare("select * from merch_messages_v2 where " + showParams.join(" or "))
     .bind(...latest5)
     .all()
     .then(r => r.results);
