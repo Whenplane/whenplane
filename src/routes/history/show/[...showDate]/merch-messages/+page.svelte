16df07ff024cbd9dba2445ddf28ff862ab5ca8e0
ajgeiss0702 <aj@ajg0702.us>
2026-01-02 19:59:02 -0700

    add affiliation disclaimer to new mm page

--- src/routes/history/show/[...showDate]/merch-messages/+page.svelte
+++ src/routes/history/show/[...showDate]/merch-messages/+page.svelte
@@ -119,6 +119,12 @@
     <Incomplete/>
   {/if}
 </div>
+<br>
+<div class="p-4">
+  <span class="text-sm opacity-60">
+    Whenplane and the Whenplane Merch Messages Index is not affiliated with or endorsed by Linus Media Group.
+  </span>
+</div>
 
 <style>
     .thumbnail-backdrop {
