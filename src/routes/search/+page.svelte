1a26bdae10de3c69c2e30ebd052d10c027bc8c08
ajgeiss0702 <aj@ajg0702.us>
2026-01-23 15:24:29 -0700

    update link to merch messages in search results

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -251,7 +251,7 @@
           {@const cleanedId = hit.document.id.replaceAll("topic-", "")}
           {@const baseShowUrl = "/history/show/" + (show ? show.name : `${hit.document.videoId}` )}
           {@const strippedSnippet = hit.highlight?.text?.snippet?.replaceAll("<mark>", "").replaceAll("</mark>", "")}
-          {@const href = (hit.document.type === "message" || hit.document.type === "reply") ? `merch-messages/${hit.document.videoId}#${hit.document.videoId}.${hit.document.imageIndex}` : baseShowUrl + (hit.document.type.startsWith("transcript") ? "/transcript?find=" + encodeURIComponent(strippedSnippet) + "#match" : "") + (hit.document.type === "topic" ? (show ? "#timestamp-" + cleanedId : `?hash=${encodeURIComponent('#timestamp-' + cleanedId)}` ) : "")}
+          {@const href = (hit.document.type === "message" || hit.document.type === "reply") ? `/history/show/${hit.document.show}/merch-messages#${hit.document.show}-${Math.floor(hit.document.timestamp)}` : baseShowUrl + (hit.document.type.startsWith("transcript") ? "/transcript?find=" + encodeURIComponent(strippedSnippet) + "#match" : "") + (hit.document.type === "topic" ? (show ? "#timestamp-" + cleanedId : `?hash=${encodeURIComponent('#timestamp-' + cleanedId)}` ) : "")}
           <span class="opacity-70">
             {#if hit.document.type === "topic"}
               Topic
