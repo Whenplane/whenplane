17a3f8a42513ab8cd8f9ba63d49394531b004e31
ajgeiss0702 <aj@ajg0702.us>
2026-01-31 12:16:07 -0700

    fix tests

--- src/vitests/timeUtils/nextWAN.test.ts
+++ src/vitests/timeUtils/nextWAN.test.ts
@@ -1,20 +1,24 @@
 import { expect, test } from 'vitest'
 import {getNextWAN} from "../../lib/timeUtils";
 
-test('late show with hasDone=false', () => {
+const alternateStartTimes = await fetch(
+  'https://whenplane.com/api/alternateStartTimes'
+).then((r) => r.json());
+
+test('late show with hasDone=false', async () => {
     const testDate = new Date("2023-07-15T04:37:24.906Z") // 11:37 pm
-    const next = getNextWAN(testDate, true, false);
+    const next = getNextWAN(testDate, true, await alternateStartTimes, false);
     expect(next.getUTCDate()).toEqual(14);
 })
 
-test('late show with hasDone=true', () => {
+test('late show with hasDone=true', async () => {
     const testDate = new Date("2023-07-15T04:37:24.906Z") // 11:37 pm
-    const next = getNextWAN(testDate, true, true);
+    const next = getNextWAN(testDate, true, await alternateStartTimes, true);
     expect(next.getUTCDate()).toEqual(21);
 })
 
-test('LTX 2023 exception', () => {
+test('LTX 2023 exception', async () => {
     const testDate = new Date("2023-07-27T04:37:24.906Z")
-    const next = getNextWAN(testDate, true, true);
+    const next = getNextWAN(testDate, true, await alternateStartTimes, true);
     expect(next.getUTCDate()).toEqual(29);
 })
