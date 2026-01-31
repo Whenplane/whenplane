a673ef3a49afe60dd3e62eb66ad904d8a7c95156
ajgeiss0702 <aj@ajg0702.us>
2026-01-31 12:17:16 -0700

    why didnt i do this before instead of casting it after lol

--- src/lib/server/MfResponseConverter.ts
+++ src/lib/server/MfResponseConverter.ts
@@ -3,7 +3,7 @@ import { dev } from "$app/environment";
 export async function createMFResponse(response: Response) {
   if (dev) {
     const { realCreateMFResponse } = await import("./clients/miniflare");
-    return realCreateMFResponse(response)
+    return await realCreateMFResponse(response) as Response
   }
   return response;
 }
