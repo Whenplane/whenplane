254547eea4ceaf0db5bf83114adb1788bcc1d9a5
ajgeiss0702 <aj@ajg0702.us>
2026-02-01 13:57:55 -0700

    wip upgrading skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,8 @@
 	import { run } from 'svelte/legacy';
 	import { typed } from '$lib';
 
-	// The ordering of these imports is critical to your app working properly
-	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
-	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
-	import '@skeletonlabs/skeleton/styles/all.css';
+
+
 	// Most of your app wide CSS should be put in this file
 	import '../app.css';
 
