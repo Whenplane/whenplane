4f896266091ca40628f6ff1f51cb9816fb24f330
ajgeiss0702 <aj@ajg0702.us>
2026-03-09 13:57:57 -0700

    fix more dropdown causing page to jump

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -336,7 +336,7 @@
 					<span class="capitalize">More</span>
 					<span><CaretDownFill/></span>
 				</button>
-				<div class="card w-58 shadow-2xl! overflow-hidden z-20 !bg-surface-500" data-popup="moreDropdown" style="opacity: 0; pointer-events: none; display: none;">
+				<div class="card w-58 shadow-2xl! overflow-hidden z-20 !bg-surface-500 absolute" data-popup="moreDropdown" style="opacity: 0; pointer-events: none; display: none;">
 					<MoreLinks/>
 
 					<div class="arrow bg-surface-500"></div>
