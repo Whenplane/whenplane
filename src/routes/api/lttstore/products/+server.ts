283de7e3d9fd7ed94f06e290adac899eb331b399
ajgeiss0702 <aj@ajg0702.us>
2026-03-16 14:05:59 -0700

    add all products endpoint

---
+++ src/routes/api/lttstore/products/+server.ts
@@ -0,0 +1,17 @@
+import { retryD1 } from "$lib/utils.ts";
+import type { ProductsTableRow } from "$lib/lttstore/lttstore_types.ts";
+import { error, json, type RequestHandler } from "@sveltejs/kit";
+
+export const GET = (async ({platform}) => {
+
+  const db = platform?.env?.LTTSTORE_DB.withSession();
+  if(!db) throw error(503, "DB unavailable!");
+
+  const allProducts = retryD1(() =>
+    db.prepare("select handle,id,available,json_remove(json_remove(json_remove(json_remove(product, '$.media'), '$.images'), '$.variants'), '$.description') as product from products")
+      .all<ProductsTableRow>()
+      .then(r => r.results)
+  );
+
+  return json(allProducts);
+}) satisfies RequestHandler;
