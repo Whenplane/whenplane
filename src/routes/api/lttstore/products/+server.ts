1bb4fc66eb7281f1c3ca547be6d45132ab114c18
ajgeiss0702 <aj@ajg0702.us>
2026-03-16 14:57:03 -0700

    add option to not remove variants

--- src/routes/api/lttstore/products/+server.ts
+++ src/routes/api/lttstore/products/+server.ts
@@ -2,13 +2,21 @@ import { retryD1 } from "$lib/utils.ts";
 import type { ProductsTableRow } from "$lib/lttstore/lttstore_types.ts";
 import { error, json, type RequestHandler } from "@sveltejs/kit";
 
-export const GET = (async ({platform}) => {
+export const GET = (async ({platform, url}) => {
 
   const db = platform?.env?.LTTSTORE_DB.withSession();
   if(!db) throw error(503, "DB unavailable!");
+  const withVariants = url.searchParams.has("withVariants");
 
   const allProducts = await retryD1(() =>
-    db.prepare("select handle,id,available,json_remove(json_remove(json_remove(json_remove(product, '$.media'), '$.images'), '$.variants'), '$.description') as product from products")
+    db.prepare(
+      "select handle,id,available," +
+      (withVariants ?
+         "json_remove(json_remove(json_remove(product, '$.media'), '$.images'), '$.description')" :
+         "json_remove(json_remove(json_remove(json_remove(product, '$.media'), '$.images'), '$.description'), '$.variants')"
+      ) +
+      " as product from products"
+    )
       .all<ProductsTableRow>()
       .then(r => r.results)
   );
