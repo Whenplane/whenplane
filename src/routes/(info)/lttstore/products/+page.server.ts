d26f05da3db0c602533756c9ccd67b88488ca775
ajgeiss0702 <aj@ajg0702.us>
2026-04-12 15:39:45 -0700

    put not-available products later

--- src/routes/(info)/lttstore/products/+page.server.ts
+++ src/routes/(info)/lttstore/products/+page.server.ts
@@ -31,7 +31,9 @@ export const load = (async ({platform, url}) => {
   }
 
   const allProducts = retryD1(() =>
-    db.prepare("select handle,shortTitle,id,available,json_remove(json_remove(json_remove(json_remove(product, '$.media'), '$.images'), '$.variants'), '$.description') as product from products order by " + sortColumn + " DESC")
+    // a bunch of stuff removed that isnt needed for the products page
+    // ordered by available second to put items currently on lttstore above ones that arent
+    db.prepare("select handle,shortTitle,id,available,json_remove(json_remove(json_remove(json_remove(product, '$.media'), '$.images'), '$.variants'), '$.description') as product from products order by " + sortColumn + " DESC, available DESC")
       .all<ProductsTableRow>()
       .then(r => r.results)
   );
