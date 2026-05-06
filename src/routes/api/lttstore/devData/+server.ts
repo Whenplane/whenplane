166276913eeb054693a9909f6188ef398cb7f024
ajgeiss0702 <aj@ajg0702.us>
2026-05-06 16:42:19 -0700

    try 20k

--- src/routes/api/lttstore/devData/+server.ts
+++ src/routes/api/lttstore/devData/+server.ts
@@ -32,7 +32,7 @@ export const GET = (async ({platform}) => {
     .finally(() => console.log("screwdriverStocks query finished"))
 
   // const waterBottleChanges = db.prepare("select * from change_history where id = 7117650296935")
-  const changeHistory = db.prepare("select * from change_history where id = 6649895092327 or timestamp > ? limit 30000")
+  const changeHistory = db.prepare("select * from change_history where id = 6649895092327 or timestamp > ? limit 20000")
     .bind(Date.now() - (14 * 24 * 60 * 60e3)) // only get non-screwdriver changes from the past 14 days
     .all()
     .then(r => r.results)
