5e92ad6f59f4c46ba781558d754f86dabc3a1000
ajgeiss0702 <aj@ajg0702.us>
2026-06-18 12:51:14 -0700

    allow flags to scale down if screen is too small

--- src/routes/(info)/lttstore/+page.svelte
+++ src/routes/(info)/lttstore/+page.svelte
@@ -7,11 +7,13 @@
     <h1>Please select a store</h1>
     <div class="flex justify-center items-center gap-2 flex-wrap">
       <a href="/lttstore/us">
-        <img class="w-64 aspect-19/10 object-fill" src={USFlag} alt="US" />
-        US
+        <img class="max-w-64 w-[95dvw] aspect-19/10 object-fill" src={USFlag} alt="US" />
+        United Sates
       </a>
       <a href="/lttstore/global">
-        <Global/>
+        <div class="max-w-64 w-[95dvw]">
+          <Global/>
+        </div>
         Global
       </a>
     </div>
--- src/routes/(info)/lttstore/flags/Global.svelte
+++ src/routes/(info)/lttstore/flags/Global.svelte
@@ -2,6 +2,6 @@
 	import Globe from "svelte-bootstrap-icons/lib/Globe.svelte";
 
 </script>
-<div class="w-64 aspect-19/10 bg-blue-500 flex justify-center items-center text-white">
+<div class="w-full aspect-19/10 bg-blue-500 flex justify-center items-center text-white">
   <Globe class="aspect-square h-[80%]" width="512" height="512"/>
 </div>
