af0b88fe6924139eb0f9178cc5d675a696d775b8
ajgeiss0702 <aj@ajg0702.us>
2026-06-18 16:59:15 -0700

    add title and description to store selector page

--- src/routes/(info)/lttstore/+page.svelte
+++ src/routes/(info)/lttstore/+page.svelte
@@ -2,10 +2,14 @@
   import USFlag from "./flags/us.webp"
   import Global from "./flags/Global.svelte";
 </script>
+<svelte:head>
+  <title>Select a Store - Whenplane LTTStore Watcher</title>
+  <meta name="description" content="Select a store to go to the Whenplane LTTStore Watcher page for that store.">
+</svelte:head>
 <div class="min-h-dvh flex justify-center items-center text-center px-4">
   <div>
     <h1>Please select a store</h1>
-    <div class="flex justify-center items-center gap-4 flex-wrap">
+    <div class="my-4 flex justify-center items-center gap-4 flex-wrap">
       <a href="/lttstore/us">
         <img class="max-w-75 w-[95dvw] aspect-19/10 object-fill" src={USFlag} alt="US" />
         United Sates
@@ -17,11 +21,26 @@
         Global
       </a>
     </div>
+    <div class="limit mt-8 opacity-70">
+      <p>
+        Since LTTStore is separated into two separate storefronts, each with their own products and stock,
+        Whenplane tracks them separately.
+        You can switch between them any time by either going to this page again,
+        or there is a button to switch to the other store on the next page.
+      </p>
+      <p>
+        Products from the opposite store may show up in some places (e.g. similar products)
+        where they will be marked with the flag (like above) of the opposite store.
+      </p>
+    </div>
   </div>
 </div>
 <style>
     @reference "#app.css";
     a {
-        @apply text-inherit card;
+        @apply block text-inherit;
+    }
+    p {
+        @apply my-1.5;
     }
 </style>
