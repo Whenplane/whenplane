a0fdd3b856711e0c184eeabd1ab968d429495e3c
ajgeiss0702 <aj@ajg0702.us>
2026-06-24 12:44:04 -0700

    add pagination to product changeHistory api

--- src/routes/api/lttstore/[store]/products/[handle]/changeHistory/+server.ts
+++ src/routes/api/lttstore/[store]/products/[handle]/changeHistory/+server.ts
@@ -4,8 +4,9 @@ import { createTables } from "../../../../../../(info)/lttstore/createTables.ts"
 import type {RequestHandler} from "./$types";
 import { storeIdFromName } from "$lib/lttstore/lttstore_types.ts";
 
+const PAGE_SIZE = 100;
 
-export const GET = (async ({fetch, params, platform}) => {
+export const GET = (async ({fetch, params, platform, url}) => {
   const db = platform?.env?.LTTSTORE_DB.withSession();
   if(!db) throw error(503, "DB unavailable!");
 
@@ -18,10 +19,36 @@ export const GET = (async ({fetch, params, platform}) => {
 
   const store = storeIdFromName(params.store);
 
-  const changeHistory = await db.prepare("select * from change_history where store = ? and id = ? order by timestamp desc")
-    .bind(store, brief.id)
-    .all<{id: number, timestamp: number, field: string, old: string, new: string}>()
-    .then(r => r.results);
+  const offset = Number(url.searchParams.get("offset") ?? 0);
+  if(isNaN(offset)) throw error(400, "Invalid offset! Must be a number");
 
-  return json(changeHistory);
+  const results = await db
+    .prepare("select * from change_history where store = ? and id = ? order by timestamp desc limit ? offset ?")
+    .bind(
+      store,
+      brief.id,
+      PAGE_SIZE + 1, // pre-fetch 1 extra to see if there is more than the current page
+      offset
+    )
+    .all<{id: number, timestamp: number, field: string, old: string, new: string}>()
+    .then(r => r.results)
+
+  const hasNextPage = results.length > PAGE_SIZE;
+  const changeHistory = hasNextPage ? results.slice(0, PAGE_SIZE) : results;
+
+  const nextOffset = hasNextPage ? offset + PAGE_SIZE : undefined;
+
+  return json({
+    changeHistory,
+    page: {
+      perPage: PAGE_SIZE,
+      hasNextPage,
+      nextOffset,
+      hint: url.searchParams.has("offset") && hasNextPage
+        ? undefined
+        : `Add ?offset=${nextOffset} to the url to get the next page. ` +
+        `Then keep using nextOffset to get the next page until hasNextPage is false ` +
+        `(where nextOffset would also be undefined)`
+    }
+  });
 }) satisfies RequestHandler
