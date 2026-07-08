f19d8bf6fcb78476de25a250a2f462845a9909eb
ajgeiss0702 <aj@ajg0702.us>
2026-07-08 15:04:33 -0700

    reject totals more than 1m almost certainly an error

--- src/routes/(info)/lttstore/totals/TotalMoveRateGraph.svelte
+++ src/routes/(info)/lttstore/totals/TotalMoveRateGraph.svelte
@@ -64,6 +64,7 @@
               const stockDiff = previousStock - currentStock;
               const rate = stockDiff / (timeDiff / (60 * 60e3));
               if(rate < 0) return null;
+              if(rate > 1_000_000) return null;
               return rate;
             } else {
               return null;
