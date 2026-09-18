f92c7614d46a7f6dd4542bcfc96db55e121a455d
ajgeiss0702 <aj@ajg0702.us>
2026-09-18 12:38:53 -0700

    Update sammy's username again

--- src/routes/api/(live-statuses)/notable-streams/notable-people.ts
+++ src/routes/api/(live-statuses)/notable-streams/notable-people.ts
@@ -2,6 +2,6 @@ export const notablePeople: { [channel: string]: string } = {
   "bocabola": "Elijah",
   "buhdan": "Dan",
   "luke_lafr": "Luke",
-  "imkasino_": "Sammy"
+  "v3sammy": "Sammy"
   // "ajgeiss0702": "Test"
 }
